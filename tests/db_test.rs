ee2a2454464acf21e887ceceeae53ab099b3ef36
michaelvlach <vlach.michael@gmail.com>
2022-08-19 22:40:04 +0200

    Update db_test.rs

--- tests/db_test.rs
+++ tests/db_test.rs
@@ -1,6 +1,15 @@
+use agdb::Query;
+
 extern crate agdb;
 
 #[test]
 fn db_is_public_type() {
     let _db = agdb::Db::default();
+}
+
+#[test]
+fn exec_takes_query_returns_query_result() {
+    let db = agdb::Db::default();
+    let query = agdb::Query::default();
+    let _result: agdb::QueryResult = db.exec(query);
 }

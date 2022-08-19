64413bf67adf009f85f88b4de0c266e6b1ca5c9e
Michael Vlach <vlach.michael@gmail.com>
2022-08-19 22:43:13 +0200

    [db] Add Db::exec() function #33 (#34)
    * Update db_test.rs
    
    * Update db.rs

--- tests/db_test.rs
+++ tests/db_test.rs
@@ -4,3 +4,10 @@ extern crate agdb;
 fn db_is_public_type() {
     let _db = agdb::Db::default();
 }
+
+#[test]
+fn exec_takes_query_returns_query_result() {
+    let db = agdb::Db::default();
+    let query = agdb::Query::default();
+    let _result: agdb::QueryResult = db.exec(query);
+}

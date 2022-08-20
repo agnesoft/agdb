77a798417b3128582d30eaefdccdcd26c9b91ceb
michaelvlach <vlach.michael@gmail.com>
2022-08-20 11:13:15 +0200

    Update transaction_test.rs

--- tests/transaction_test.rs
+++ tests/transaction_test.rs
@@ -10,3 +10,11 @@ fn create_transaction_from_db() {
     let db = agdb::Db::default();
     let _transaction: agdb::Transaction = db.transaction();
 }
+
+#[test]
+fn exec_takes_query_returns_query_result() {
+    let db = agdb::Db::default();
+    let query = agdb::Query::default();
+    let transaction = db.transaction();
+    let _result: agdb::QueryResult = transaction.exec(query);
+}

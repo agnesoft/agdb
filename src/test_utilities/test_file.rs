9f8258bddd8cdcb50058bcce1c0c182d7dfbca30
Michael Vlach <vlach.michael@gmail.com>
2022-08-20 23:56:37 +0200

    [tests] Add TestFile::file_name() #56 (#57)
    * Update test_file.rs
    
    * Update test_file.rs

--- src/test_utilities/test_file.rs
+++ src/test_utilities/test_file.rs
@@ -10,6 +10,13 @@ fn remove_file_if_exists(filename: &String) {
     }
 }
 
+impl TestFile {
+    #[allow(dead_code)]
+    pub(crate) fn file_name(&self) -> &String {
+        &self.filename
+    }
+}
+
 impl From<&str> for TestFile {
     fn from(filename: &str) -> Self {
         TestFile::from(filename.to_string())
@@ -33,43 +40,53 @@ impl Drop for TestFile {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs::File;
     use std::path::Path;
+
+    fn ensure_file(filename: &str) {
+        std::fs::OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .open(filename)
+            .unwrap();
+    }
 
     #[test]
     fn created_from_str_ref() {
-        let filename = "./test_file_test_file";
+        let filename = "./test_file_test_file01";
         let _test_file = TestFile::from(filename);
     }
 
     #[test]
     fn created_from_string() {
-        let filename = "./test_file_test_file".to_string();
+        let filename = "./test_file_test_file02".to_string();
         let _test_file = TestFile::from(filename);
+    }
+
+    #[test]
+    fn get_file_name() {
+        let filename = "./test_file_test_file03";
+        let test_file = TestFile::from(filename);
+
+        assert_eq!(test_file.file_name(), filename);
     }
 
     #[test]
     fn existing_file_is_deleted_on_construction() {
-        let filename = "./test_file_test_file";
-        let file_path = Path::new(filename);
-        File::create(filename).unwrap();
-        assert!(file_path.exists());
-
+        let filename = "./test_file_test_file04";
+        ensure_file(filename);
         let _test_file = TestFile::from(filename);
-        assert!(!file_path.exists());
+        assert!(!Path::new(filename).exists());
     }
 
     #[test]
     fn file_is_deleted_on_destruction() {
-        let filename = "./test_file_test_file";
-        let file_path = Path::new(filename);
+        let filename = "./test_file_test_file05";
 
         {
             let _test_file = TestFile::from(filename);
-            File::create(filename).unwrap();
-            assert!(file_path.exists());
+            ensure_file(filename);
         }
 
-        assert!(!file_path.exists());
+        assert!(!Path::new(filename).exists());
     }
 }

977185c13f3aeca52403a10d58de27fa41aa27dc
michaelvlach <vlach.michael@gmail.com>
2022-08-20 23:24:37 +0200

    Update file_storage.rs

--- src/storage/file_storage.rs
+++ src/storage/file_storage.rs
@@ -1,10 +1,25 @@
 #[allow(dead_code)]
 #[derive(Default)]
-pub(crate) struct FileStorage {}
+pub(crate) struct FileStorage {
+    filename: String,
+}
+
+impl From<&str> for FileStorage {
+    fn from(filename: &str) -> Self {
+        FileStorage::from(filename.to_string())
+    }
+}
+
+impl From<String> for FileStorage {
+    fn from(filename: String) -> Self {
+        FileStorage { filename }
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::test_utilities::test_file::TestFile;
 
     #[test]
     fn file_storage() {
@@ -13,6 +28,7 @@ mod tests {
 
     #[test]
     fn create_new_file() {
-        let filename = "./file_storage_test.agdb";
+        let test_file = TestFile::from("./file_storage_test.agdb");
+        let _storage = FileStorage::from(test_file.filename);
     }
 }

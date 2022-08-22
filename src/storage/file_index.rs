212224f0db089c21ef2074845241e0819b0ef06e
Michael Vlach <vlach.michael@gmail.com>
2022-08-22 22:17:51 +0200

    [storage] Add free index to FileIndex #65 (#66)
    * Update file_index.rs
    
    * Update file_index.rs
    
    * Update file_index.rs

--- src/storage/file_index.rs
+++ src/storage/file_index.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 #[derive(Default)]
 pub(crate) struct FileIndex {
     positions: HashMap<i64, u64>,
+    free_list: Vec<i64>,
 }
 
 #[allow(dead_code)]
@@ -12,12 +13,20 @@ impl FileIndex {
         self.positions.get(&index)
     }
 
-    pub(crate) fn insert(&mut self, index: i64, position: u64) {
+    pub(crate) fn create(&mut self, position: u64) -> i64 {
+        let mut index = self.positions.len() as i64;
+
+        if let Some(free_index) = self.free_list.pop() {
+            index = free_index;
+        }
+
         self.positions.insert(index, position);
+        index
     }
 
     pub(crate) fn remove(&mut self, index: i64) {
         self.positions.remove(&index);
+        self.free_list.push(index);
     }
 }
 
@@ -31,15 +40,13 @@ mod tests {
     }
 
     #[test]
-    fn insert_indexes() {
+    fn create_indexes() {
         let mut file_index = FileIndex::default();
-        let index1 = 1_i64;
-        let index2 = 2_i64;
         let pos1 = 32u64;
         let pos2 = 64u64;
 
-        file_index.insert(index1, pos1);
-        file_index.insert(index2, pos2);
+        let index1 = file_index.create(pos1);
+        let index2 = file_index.create(pos2);
 
         assert_eq!(file_index.get(index1), Some(&pos1));
         assert_eq!(file_index.get(index2), Some(&pos2));
@@ -48,16 +55,31 @@ mod tests {
     #[test]
     fn remove_index() {
         let mut file_index = FileIndex::default();
-        let index1 = 1_i64;
-        let index2 = 2_i64;
         let pos1 = 32u64;
         let pos2 = 64u64;
+        let index1 = file_index.create(pos1);
+        let index2 = file_index.create(pos2);
 
-        file_index.insert(index1, pos1);
-        file_index.insert(index2, pos2);
         file_index.remove(index1);
 
         assert_eq!(file_index.get(index1), None);
         assert_eq!(file_index.get(index2), Some(&pos2));
+    }
+
+    #[test]
+    fn reuse_indexes() {
+        let mut file_index = FileIndex::default();
+        let pos1 = 32u64;
+        let pos2 = 64u64;
+        let index1 = file_index.create(pos1);
+        let index2 = file_index.create(pos2);
+
+        file_index.remove(index1);
+        file_index.remove(index2);
+        let index3 = file_index.create(pos1);
+        let index4 = file_index.create(pos2);
+
+        assert_eq!(index3, index2);
+        assert_eq!(index4, index1);
     }
 }

6244e9e74a3d9bac0c376c340805bc56fd2a2944
michaelvlach <vlach.michael@gmail.com>
2022-08-23 13:02:33 +0200

    Update serialize.rs

--- src/storage/serialize.rs
+++ src/storage/serialize.rs
@@ -1,11 +1,14 @@
+use std::mem::size_of;
+
 pub(crate) trait Serialize {
-    fn deserialize(bytes: Vec<u8>) -> Self;
+    fn deserialize(bytes: &[u8]) -> Self;
     fn serialize(&self) -> Vec<u8>;
 }
 
 impl Serialize for i64 {
-    fn deserialize(bytes: Vec<u8>) -> Self {
-        i64::from_le_bytes(bytes.try_into().unwrap())
+    fn deserialize(bytes: &[u8]) -> Self {
+        const END: usize = size_of::<i64>();
+        i64::from_le_bytes((&bytes[0..END]).try_into().unwrap())
     }
 
     fn serialize(&self) -> Vec<u8> {
@@ -14,8 +17,9 @@ impl Serialize for i64 {
 }
 
 impl Serialize for u64 {
-    fn deserialize(bytes: Vec<u8>) -> Self {
-        u64::from_le_bytes(bytes.try_into().unwrap())
+    fn deserialize(bytes: &[u8]) -> Self {
+        const END: usize = size_of::<u64>();
+        u64::from_le_bytes((&bytes[0..END]).try_into().unwrap())
     }
 
     fn serialize(&self) -> Vec<u8> {
@@ -31,7 +35,7 @@ mod tests {
     fn i64() {
         let number = -10_i64;
         let bytes = number.serialize();
-        let actual = i64::deserialize(bytes);
+        let actual = i64::deserialize(&bytes);
 
         assert_eq!(actual, number)
     }
@@ -40,7 +44,7 @@ mod tests {
     fn u64() {
         let number = 10_u64;
         let bytes = number.serialize();
-        let actual = u64::deserialize(bytes);
+        let actual = u64::deserialize(&bytes);
 
         assert_eq!(actual, number)
     }

c99ccb0131224f226b7e38db6f3984d73c7963d9
michaelvlach <vlach.michael@gmail.com>
2022-08-23 11:27:55 +0200

    Update serialize.rs

--- src/storage/serialize.rs
+++ src/storage/serialize.rs
@@ -1,3 +1,28 @@
+pub(crate) trait Serialize {
+    fn deserialize(bytes: Vec<u8>) -> Self;
+    fn serialize(&self) -> Vec<u8>;
+}
+
+impl Serialize for i64 {
+    fn deserialize(bytes: Vec<u8>) -> Self {
+        i64::from_le_bytes(bytes.try_into().unwrap())
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        self.to_le_bytes().to_vec()
+    }
+}
+
+impl Serialize for u64 {
+    fn deserialize(bytes: Vec<u8>) -> Self {
+        u64::from_le_bytes(bytes.try_into().unwrap())
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        self.to_le_bytes().to_vec()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;

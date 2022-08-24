a68b0f3dd238901210c6cc167925762ad452223a
Michael Vlach <vlach.michael@gmail.com>
2022-08-24 11:25:57 +0200

    [storage] Add seek to FileWrapper #92 (#93)
    * Update file_wrapper.rs
    
    * Update file_wrapper.rs

--- src/storage/file_wrapper.rs
+++ src/storage/file_wrapper.rs
@@ -12,14 +12,24 @@ pub(crate) struct FileWrapper {
 
 #[allow(dead_code)]
 impl FileWrapper {
+    pub(crate) fn current_pos(&mut self) -> u64 {
+        self.file.seek(SeekFrom::Current(0)).expect(ERROR_MESSAGE)
+    }
+
     pub(crate) fn read(&mut self, size: u64) -> Vec<u8> {
         let mut buffer = vec![0_u8; size as usize];
         self.file.read_exact(&mut buffer).expect(ERROR_MESSAGE);
         buffer
     }
 
-    pub(crate) fn current_pos(&mut self) -> u64 {
-        self.file.seek(SeekFrom::Current(0)).expect(ERROR_MESSAGE)
+    pub(crate) fn seek(&mut self, position: u64) {
+        self.file
+            .seek(SeekFrom::Start(position))
+            .expect(ERROR_MESSAGE);
+    }
+
+    pub(crate) fn seek_end(&mut self) {
+        self.file.seek(SeekFrom::End(0)).expect(ERROR_MESSAGE);
     }
 
     pub(crate) fn write(&mut self, data: &[u8]) {
@@ -51,7 +61,8 @@ mod tests {
     use super::*;
     use crate::storage::serialize::Serialize;
     use crate::test_utilities::test_file::TestFile;
-    use std::{io::Write, mem::size_of, path::Path};
+    use std::mem::size_of;
+    use std::path::Path;
 
     #[test]
     fn create_new_file() {
@@ -64,19 +75,6 @@ mod tests {
     }
 
     #[test]
-    fn current_pos() {
-        let test_file = TestFile::from("./file_storage_test03.agdb");
-        let mut file = FileWrapper::from(test_file.file_name().clone());
-
-        assert_eq!(file.current_pos(), 0);
-
-        let data = 10_i64.serialize();
-        file.file.write_all(&data).unwrap();
-
-        assert_eq!(file.current_pos(), size_of::<i64>() as u64);
-    }
-
-    #[test]
     fn open_existing_file() {
         let test_file = TestFile::from("./file_storage_test02.agdb");
         File::create(test_file.file_name()).unwrap();
@@ -84,13 +82,36 @@ mod tests {
     }
 
     #[test]
-    fn write_read_bytes() {
+    fn seek() {
         let test_file = TestFile::from("./file_storage_test03.agdb");
+        let mut file = FileWrapper::from(test_file.file_name().clone());
+
+        assert_eq!(file.current_pos(), 0);
+        file.seek(10);
+        assert_eq!(file.current_pos(), 10);
+    }
+
+    #[test]
+    fn seek_end() {
+        let test_file = TestFile::from("./file_storage_test03.agdb");
+        let mut file = FileWrapper::from(test_file.file_name().clone());
+        let data = 10_i64.serialize();
+        file.write(&data);
+        file.seek(0);
+
+        assert_eq!(file.current_pos(), 0);
+        file.seek_end();
+        assert_eq!(file.current_pos(), size_of::<i64>() as u64);
+    }
+
+    #[test]
+    fn write_read_bytes() {
+        let test_file = TestFile::from("./file_storage_test04.agdb");
         let mut file = FileWrapper::from(test_file.file_name().clone());
         let data = 10_i64.serialize();
 
         file.write(&data);
-        file.file.seek(SeekFrom::Start(0)).unwrap();
+        file.seek(0);
 
         let actual_data = file.read(size_of::<i64>() as u64);
 

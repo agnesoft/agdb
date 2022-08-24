67eaac210236014f45ff072a60b29cd41d7a09cd
michaelvlach <vlach.michael@gmail.com>
2022-08-24 10:47:50 +0200

    Update file_storage.rs

--- src/storage/file_storage.rs
+++ src/storage/file_storage.rs
@@ -1,8 +1,7 @@
 use super::file_record::FileRecord;
 use super::file_records::FileRecords;
+use super::file_wrapper::FileWrapper;
 use super::serialize::Serialize;
-use std::fs::File;
-use std::fs::OpenOptions;
 use std::io::Read;
 use std::io::Write;
 use std::io::{Seek, SeekFrom};
@@ -10,29 +9,29 @@ use std::mem::size_of;
 
 #[allow(dead_code)]
 pub(crate) struct FileStorage {
-    filename: String,
-    file: File,
+    file: FileWrapper,
     records: FileRecords,
 }
 
 #[allow(dead_code)]
 impl FileStorage {
     pub(crate) fn insert<T: Serialize>(&mut self, value: &T) -> i64 {
-        let position = self.file.seek(SeekFrom::End(0)).unwrap();
+        let position = self.file.file.seek(SeekFrom::End(0)).unwrap();
         let bytes = value.serialize();
         let record = self.records.create(position, bytes.len() as u64);
-        self.file.write_all(&record.serialize()).unwrap();
-        self.file.write_all(&bytes).unwrap();
+        self.file.file.write_all(&record.serialize()).unwrap();
+        self.file.file.write_all(&bytes).unwrap();
         record.index
     }
 
     pub(crate) fn value<T: Serialize>(&mut self, index: i64) -> Option<T> {
         if let Some(record) = self.records.get(index) {
             self.file
+                .file
                 .seek(SeekFrom::Start(record.position + FileRecord::size() as u64))
                 .unwrap();
             let mut bytes: Vec<u8> = vec![0; record.size as usize];
-            self.file.read_exact(&mut bytes).unwrap();
+            self.file.file.read_exact(&mut bytes).unwrap();
             return Some(T::deserialize(&bytes));
         }
 
@@ -42,12 +41,13 @@ impl FileStorage {
     pub(crate) fn value_at<T: Serialize>(&mut self, index: i64, offset: u64) -> Option<T> {
         if let Some(record) = self.records.get(index) {
             self.file
+                .file
                 .seek(SeekFrom::Start(
                     record.position + FileRecord::size() as u64 + offset,
                 ))
                 .unwrap();
             let mut bytes: Vec<u8> = vec![0; size_of::<T>()];
-            self.file.read_exact(&mut bytes).unwrap();
+            self.file.file.read_exact(&mut bytes).unwrap();
             return Some(T::deserialize(&bytes));
         }
 
@@ -63,16 +63,8 @@ impl From<&str> for FileStorage {
 
 impl From<String> for FileStorage {
     fn from(filename: String) -> Self {
-        let file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .read(true)
-            .open(&filename)
-            .unwrap();
-
         FileStorage {
-            filename,
-            file,
+            file: FileWrapper::from(filename),
             records: FileRecords::default(),
         }
     }
@@ -82,15 +74,6 @@ impl From<String> for FileStorage {
 mod tests {
     use super::*;
     use crate::test_utilities::test_file::TestFile;
-    use std::path::Path;
-
-    #[test]
-    fn create_new_file() {
-        let test_file = TestFile::from("./file_storage_test01.agdb");
-        let _storage = FileStorage::from(test_file.file_name().as_str());
-
-        assert!(Path::new(test_file.file_name()).exists());
-    }
 
     #[test]
     fn insert() {
@@ -100,13 +83,6 @@ mod tests {
         let index = storage.insert(&10_i64);
 
         assert_eq!(index, 0);
-    }
-
-    #[test]
-    fn open_existing_file() {
-        let test_file = TestFile::from("./file_storage_test03.agdb");
-        File::create(test_file.file_name()).unwrap();
-        let _storage = FileStorage::from(test_file.file_name().clone());
     }
 
     #[test]

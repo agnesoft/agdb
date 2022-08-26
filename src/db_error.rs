7b31de8476e2b17968b1c52337164937beb54eac
michaelvlach <vlach.michael@gmail.com>
2022-08-26 14:43:47 +0200

    wip

--- src/db_error.rs
+++ src/db_error.rs
@@ -4,8 +4,16 @@ pub(crate) enum DbError {
     Storage(String),
 }
 
+impl From<std::io::Error> for DbError {
+    fn from(error: std::io::Error) -> Self {
+        DbError::Storage(error.to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::io::ErrorKind;
+
     use super::*;
 
     #[test]
@@ -15,7 +23,7 @@ mod tests {
     }
 
     #[test]
-    fn storage() {
-        let _error = DbError::Storage("error".to_string());
+    fn from_io_error() {
+        let _error = DbError::from(std::io::Error::from(ErrorKind::NotFound));
     }
 }
--- src/storage/file_wrapper.rs
+++ src/storage/file_wrapper.rs
@@ -1,42 +1,44 @@
 use std::fs::{File, OpenOptions};
-use std::io::{Read, Seek, SeekFrom, Write};
+use std::io::{ErrorKind, Read, Seek, SeekFrom, Write};
 
 use crate::db_error::DbError;
 
 #[allow(dead_code)]
-pub(crate) struct FileWrapper {
-    pub(crate) file: File,
+pub(crate) struct FileWrapper<FileT = File>
+where
+    FileT: Read,
+    FileT: Seek,
+    FileT: Write,
+{
+    pub(crate) file: FileT,
     pub(crate) filename: String,
     pub(crate) size: u64,
 }
 
 #[allow(dead_code)]
-impl FileWrapper {
+impl<FileT> FileWrapper<FileT>
+where
+    FileT: Read,
+    FileT: Seek,
+    FileT: Write,
+{
     pub(crate) fn current_pos(&mut self) -> Result<u64, DbError> {
-        self.file
-            .seek(SeekFrom::Current(0))
-            .map_err(|error| DbError::Storage(error.to_string()))
+        Ok(self.file.seek(SeekFrom::Current(0))?)
     }
 
     pub(crate) fn read(&mut self, size: u64) -> Result<Vec<u8>, DbError> {
         let mut buffer = vec![0_u8; size as usize];
-        self.file
-            .read_exact(&mut buffer)
-            .map_err(|error| DbError::Storage(error.to_string()))?;
+        self.file.read_exact(&mut buffer)?;
         Ok(buffer)
     }
 
     pub(crate) fn seek(&mut self, position: u64) -> Result<(), DbError> {
-        self.file
-            .seek(SeekFrom::Start(position))
-            .map_err(|error| DbError::Storage(error.to_string()))?;
+        self.file.seek(SeekFrom::Start(position))?;
         Ok(())
     }
 
     pub(crate) fn seek_end(&mut self) -> Result<(), DbError> {
-        self.file
-            .seek(SeekFrom::End(0))
-            .map_err(|error| DbError::Storage(error.to_string()))?;
+        self.file.seek(SeekFrom::End(0))?;
         Ok(())
     }
 
@@ -47,9 +49,7 @@ impl FileWrapper {
             self.size = end_position;
         }
 
-        self.file
-            .write_all(data)
-            .map_err(|error| DbError::Storage(error.to_string()))
+        Ok(self.file.write_all(data)?)
     }
 }
 
@@ -61,12 +61,9 @@ impl TryFrom<String> for FileWrapper {
             .write(true)
             .create(true)
             .read(true)
-            .open(&filename)
-            .map_err(|error| DbError::Storage(error.to_string()))?;
+            .open(&filename)?;
 
-        let size = file
-            .seek(SeekFrom::End(0))
-            .map_err(|error| DbError::Storage(error.to_string()))?;
+        let size = file.seek(SeekFrom::End(0))?;
 
         Ok(FileWrapper {
             file,
@@ -178,5 +175,20 @@ mod tests {
         let actual_data = file.read(size_of::<i64>() as u64);
 
         assert_eq!(actual_data, Ok(data));
+    }
+
+    #[test]
+    fn bad_file() {
+        let test_file = TestFile::from("./file_storage_test09.agdb");
+        let mut file = FileWrapper {
+            file: BadFile {},
+            filename: test_file.file_name().clone(),
+            size: 0,
+        };
+
+        assert!(file.current_pos().is_err());
+        assert!(file.seek(0).is_err());
+        assert!(file.read(0).is_err());
+        assert!(file.write(&vec![0_u8; 0]).is_err());
     }
 }

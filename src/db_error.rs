1bc367582bab0704e2fa628d5675d2dc0d23f92e
michaelvlach <vlach.michael@gmail.com>
2022-08-27 16:33:46 +0200

    Update db_error.rs

--- src/db_error.rs
+++ src/db_error.rs
@@ -5,10 +5,18 @@ pub(crate) enum DbError {
 
 #[cfg(test)]
 mod tests {
+    use std::io::ErrorKind;
+
     use super::*;
 
     #[test]
-    fn storage() {
+    fn derived_from_debug() {
         let _error = DbError::Storage("error".to_string());
+        format!("{:?}", _error);
+    }
+
+    #[test]
+    fn from_io_error() {
+        let _error = DbError::from(std::io::Error::from(ErrorKind::NotFound));
     }
 }

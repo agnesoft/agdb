cdb5a6e7e464d82f4929ada3548f50b97ca82140
michaelvlach <vlach.michael@gmail.com>
2022-08-27 06:56:05 +0200

    Update file_wrapper.rs

--- src/storage/file_wrapper.rs
+++ src/storage/file_wrapper.rs
@@ -1,14 +1,11 @@
-use std::fs::{File, OpenOptions};
-use std::io::{ErrorKind, Read, Seek, SeekFrom, Write};
-
 use crate::db_error::DbError;
 
 #[allow(dead_code)]
-pub(crate) struct FileWrapper<FileT = File>
+pub(crate) struct FileWrapper<FileT = std::fs::File>
 where
-    FileT: Read,
-    FileT: Seek,
-    FileT: Write,
+    FileT: std::io::Read,
+    FileT: std::io::Seek,
+    FileT: std::io::Write,
 {
     pub(crate) file: FileT,
     pub(crate) filename: String,
@@ -18,12 +15,12 @@ where
 #[allow(dead_code)]
 impl<FileT> FileWrapper<FileT>
 where
-    FileT: Read,
-    FileT: Seek,
-    FileT: Write,
+    FileT: std::io::Read,
+    FileT: std::io::Seek,
+    FileT: std::io::Write,
 {
     pub(crate) fn current_pos(&mut self) -> Result<u64, DbError> {
-        Ok(self.file.seek(SeekFrom::Current(0))?)
+        Ok(self.file.seek(std::io::SeekFrom::Current(0))?)
     }
 
     pub(crate) fn read(&mut self, size: u64) -> Result<Vec<u8>, DbError> {
@@ -33,12 +30,12 @@ where
     }
 
     pub(crate) fn seek(&mut self, position: u64) -> Result<(), DbError> {
-        self.file.seek(SeekFrom::Start(position))?;
+        self.file.seek(std::io::SeekFrom::Start(position))?;
         Ok(())
     }
 
     pub(crate) fn seek_end(&mut self) -> Result<(), DbError> {
-        self.file.seek(SeekFrom::End(0))?;
+        self.file.seek(std::io::SeekFrom::End(0))?;
         Ok(())
     }
 
@@ -57,13 +54,13 @@ impl TryFrom<String> for FileWrapper {
     type Error = DbError;
 
     fn try_from(filename: String) -> Result<Self, Self::Error> {
-        let mut file = OpenOptions::new()
+        let mut file = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
             .read(true)
             .open(&filename)?;
 
-        let size = file.seek(SeekFrom::End(0))?;
+        let size = std::io::Seek::seek(&mut file, std::io::SeekFrom::End(0))?;
 
         Ok(FileWrapper {
             file,
@@ -77,16 +74,15 @@ impl TryFrom<String> for FileWrapper {
 mod tests {
     use super::*;
     use crate::storage::serialize::Serialize;
+    use crate::test_utilities::bad_file::BadFile;
     use crate::test_utilities::test_file::TestFile;
-    use std::mem::size_of;
-    use std::path::Path;
 
     #[test]
     fn create_new_file() {
         let test_file = TestFile::from("./file_wrapper_test01.agdb");
         let file = FileWrapper::try_from(test_file.file_name().clone()).unwrap();
 
-        assert!(Path::new(test_file.file_name()).exists());
+        assert!(std::path::Path::new(test_file.file_name()).exists());
         assert_eq!(&file.filename, test_file.file_name());
         assert_eq!(file.size, 0);
     }
@@ -94,7 +90,7 @@ mod tests {
     #[test]
     fn open_existing_file() {
         let test_file = TestFile::from("./file_storage_test02.agdb");
-        File::create(test_file.file_name()).unwrap();
+        std::fs::File::create(test_file.file_name()).unwrap();
         let _file = FileWrapper::try_from(test_file.file_name().clone()).unwrap();
     }
 
@@ -118,7 +114,7 @@ mod tests {
 
         assert_eq!(file.current_pos(), Ok(0));
         file.seek_end().unwrap();
-        assert_eq!(file.current_pos(), Ok(size_of::<i64>() as u64));
+        assert_eq!(file.current_pos(), Ok(std::mem::size_of::<i64>() as u64));
     }
 
     #[test]
@@ -129,7 +125,7 @@ mod tests {
 
         assert_eq!(file.size, 0);
         file.write(&data).unwrap();
-        assert_eq!(file.size, size_of::<i64>() as u64);
+        assert_eq!(file.size, std::mem::size_of::<i64>() as u64);
     }
 
     #[test]
@@ -142,7 +138,7 @@ mod tests {
         file.write(&data).unwrap();
         file.seek(0).unwrap();
         file.write(&data).unwrap();
-        assert_eq!(file.size, size_of::<i64>() as u64);
+        assert_eq!(file.size, std::mem::size_of::<i64>() as u64);
     }
 
     #[test]
@@ -154,12 +150,12 @@ mod tests {
         assert_eq!(file.size, 0);
 
         file.write(&data).unwrap();
-        file.seek((size_of::<i64>() as u64) / 2).unwrap();
+        file.seek((std::mem::size_of::<i64>() as u64) / 2).unwrap();
         file.write(&data).unwrap();
 
         assert_eq!(
             file.size,
-            size_of::<i64>() as u64 + (size_of::<i64>() as u64 / 2)
+            std::mem::size_of::<i64>() as u64 + (std::mem::size_of::<i64>() as u64 / 2)
         );
     }
 
@@ -172,23 +168,25 @@ mod tests {
         file.write(&data).unwrap();
         file.seek(0).unwrap();
 
-        let actual_data = file.read(size_of::<i64>() as u64);
+        let actual_data = file.read(std::mem::size_of::<i64>() as u64);
 
         assert_eq!(actual_data, Ok(data));
     }
 
     #[test]
-    fn bad_file() {
+    fn bad_read() {
         let test_file = TestFile::from("./file_storage_test09.agdb");
         let mut file = FileWrapper {
-            file: BadFile {},
+            file: BadFile {
+                read_result: Err(std::io::ErrorKind::Other.into()),
+                seek_result: Ok(0),
+                write_result: Ok(0),
+                flush_result: Ok(()),
+            },
             filename: test_file.file_name().clone(),
             size: 0,
         };
 
-        assert!(file.current_pos().is_err());
-        assert!(file.seek(0).is_err());
         assert!(file.read(0).is_err());
-        assert!(file.write(&vec![0_u8; 0]).is_err());
     }
 }

46413241ac3dfb4cc701fcefa221b0ba054544c7
michaelvlach <vlach.michael@gmail.com>
2022-08-29 22:07:34 +0200

    Update file_record.rs

--- src/storage/file_record.rs
+++ src/storage/file_record.rs
@@ -1,5 +1,5 @@
 #[allow(dead_code)]
-#[derive(Clone, Debug, Default, Eq, Ord, PartialOrd, PartialEq)]
+#[derive(Clone, Debug, Default, PartialEq)]
 pub(crate) struct FileRecord {
     pub(crate) position: u64,
     pub(crate) size: u64,
@@ -8,22 +8,10 @@ pub(crate) struct FileRecord {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::cmp::Ordering;
-
-    #[test]
-    fn default_constructed() {
-        let _record = FileRecord::default();
-    }
 
     #[test]
     fn derived_from_debug() {
         let record = FileRecord::default();
         format!("{:?}", record);
-    }
-
-    #[test]
-    fn derived_from_ord() {
-        let record = FileRecord::default();
-        assert_eq!(record.cmp(&record), Ordering::Equal);
     }
 }

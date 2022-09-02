9b92b9f6523d2a0e5d80d505858b72a7f4c5d165
Michael Vlach <vlach.michael@gmail.com>
2022-09-02 07:25:20 +0200

    [storage] Add write ahead log insert() #139 (#141)
    * Update write_ahead_log.rs
    
    * Update write_ahead_log.rs
    
    * Update pr.yaml

--- src/storage/write_ahead_log.rs
+++ src/storage/write_ahead_log.rs
@@ -1,8 +1,22 @@
+use super::serialize::Serialize;
+use super::write_ahead_log_record::WriteAheadLogRecord;
 use crate::db_error::DbError;
+use std::io::Write;
 
 #[allow(dead_code)]
 pub(crate) struct WriteAheadLog {
     file: std::fs::File,
+}
+
+#[allow(dead_code)]
+impl WriteAheadLog {
+    pub(crate) fn insert(&mut self, record: WriteAheadLogRecord) -> Result<(), DbError> {
+        self.file.write_all(&record.position.serialize())?;
+        self.file
+            .write_all(&(record.bytes.len() as u64).serialize())?;
+        self.file.write_all(&record.bytes)?;
+        Ok(())
+    }
 }
 
 impl TryFrom<String> for WriteAheadLog {
@@ -27,5 +41,23 @@ mod tests {
     fn filename_constructed() {
         let test_file = TestFile::from("./write_ahead_log-filename_constructed.agdb");
         WriteAheadLog::try_from(test_file.file_name().clone()).unwrap();
+    }
+
+    #[test]
+    fn insert() {
+        let test_file = TestFile::from("./write_ahead_log-insert.agdb");
+        {
+            let mut wal = WriteAheadLog::try_from(test_file.file_name().clone()).unwrap();
+            wal.insert(WriteAheadLogRecord {
+                position: 1,
+                bytes: vec![1_u8; 5],
+            })
+            .unwrap();
+        }
+
+        let actual_size = std::fs::metadata(test_file.file_name()).unwrap().len();
+        let expected_size = (std::mem::size_of::<u64>() * 2 + 5) as u64;
+
+        assert_eq!(actual_size, expected_size);
     }
 }

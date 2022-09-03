dea0971aeffe96082d076fcc6e4e4a08b991c0ff
michaelvlach <vlach.michael@gmail.com>
2022-09-03 15:42:21 +0200

    Update write_ahead_log_record.rs

--- src/storage/write_ahead_log_record.rs
+++ src/storage/write_ahead_log_record.rs
@@ -4,3 +4,14 @@ pub(crate) struct WriteAheadLogRecord {
     pub(crate) position: u64,
     pub(crate) bytes: Vec<u8>,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn derived_from_debug() {
+        let record = WriteAheadLogRecord::default();
+        format!("{:?}", record);
+    }
+}

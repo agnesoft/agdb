526b3ad0a292c1edc2db6d47be690c38586062cf
michaelvlach <vlach.michael@gmail.com>
2022-09-12 07:39:37 +0200

    Update storage_vec.rs

--- src/storage/storage_vec.rs
+++ src/storage/storage_vec.rs
@@ -464,6 +464,45 @@ mod tests {
     }
 
     #[test]
+    fn shrink_to_fit() {
+        let test_file = TestFile::from("./storage_vec-shrink_to_fit.agdb");
+        let storage = std::rc::Rc::new(std::cell::RefCell::new(
+            FileStorage::try_from(test_file.file_name().clone()).unwrap(),
+        ));
+
+        let mut vec = StorageVec::<i64>::try_from(storage).unwrap();
+        vec.push(&1).unwrap();
+        vec.push(&3).unwrap();
+        vec.push(&5).unwrap();
+
+        assert_eq!(vec.capacity(), 64);
+
+        vec.shrink_to_fit().unwrap();
+
+        assert_eq!(vec.capacity(), 3);
+
+        vec.shrink_to_fit().unwrap();
+
+        assert_eq!(vec.capacity(), 3);
+    }
+
+    #[test]
+    fn shrink_to_fit_empty() {
+        let test_file = TestFile::from("./storage_vec-shrink_to_fit_empty.agdb");
+        let storage = std::rc::Rc::new(std::cell::RefCell::new(
+            FileStorage::try_from(test_file.file_name().clone()).unwrap(),
+        ));
+
+        let mut vec = StorageVec::<i64>::try_from(storage).unwrap();
+
+        assert_eq!(vec.capacity(), 0);
+
+        vec.shrink_to_fit().unwrap();
+
+        assert_eq!(vec.capacity(), 0);
+    }
+
+    #[test]
     fn value() {
         let test_file = TestFile::from("./storage_vec-value.agdb");
         let storage = std::rc::Rc::new(std::cell::RefCell::new(

e23c314a8d7ecced7ab4a5713f79b3c7261ede4d
michaelvlach <vlach.michael@gmail.com>
2022-09-18 10:57:05 +0200

    Create storage_hash_map_meta_value.rs

---
+++ src/storage/storage_hash_map_meta_value.rs
@@ -0,0 +1,57 @@
+use crate::DbError;
+
+use super::serialize::Serialize;
+
+#[allow(dead_code)]
+#[derive(Default, Debug, PartialEq)]
+pub(crate) enum MetaValue {
+    #[default]
+    Empty,
+    Deleted,
+    Valid,
+}
+
+impl Serialize for MetaValue {
+    fn deserialize(bytes: &[u8]) -> Result<Self, DbError> {
+        match bytes.first() {
+            Some(0) => Ok(MetaValue::Empty),
+            Some(1) => Ok(MetaValue::Valid),
+            Some(2) => Ok(MetaValue::Deleted),
+            _ => return Err(DbError::from("value out of bounds")),
+        }
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        match self {
+            MetaValue::Empty => vec![0_u8],
+            MetaValue::Deleted => vec![2_u8],
+            MetaValue::Valid => vec![1_u8],
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn derived_from_default() {
+        assert_eq!(MetaValue::default(), MetaValue::Empty);
+    }
+
+    #[test]
+    fn derived_from_debug() {
+        let value = MetaValue::Deleted;
+
+        format!("{:?}", value);
+    }
+
+    #[test]
+    fn serialize() {
+        let data = vec![MetaValue::Valid, MetaValue::Empty, MetaValue::Deleted];
+        let bytes = data.serialize();
+        let other = Vec::<MetaValue>::deserialize(&bytes).unwrap();
+
+        assert_eq!(data, other);
+    }
+}

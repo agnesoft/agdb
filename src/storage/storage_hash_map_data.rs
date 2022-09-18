ef66042172008dba5d32ba18628d2088bf7ef8cc
michaelvlach <vlach.michael@gmail.com>
2022-09-18 13:35:25 +0200

    Update storage_hash_map_data.rs

--- src/storage/storage_hash_map_data.rs
+++ src/storage/storage_hash_map_data.rs
@@ -9,12 +9,37 @@ pub(crate) struct StorageHashMapData<K: Serialize, T: Serialize> {
 
 impl<K: Serialize, T: Serialize> Serialize for StorageHashMapData<K, T> {
     fn deserialize(bytes: &[u8]) -> Result<Self, crate::DbError> {
-        
+        let size = u64::deserialize(bytes)?;
+
+        const SIZE_OFFSET: usize = std::mem::size_of::<usize>();
+        let value_offset = StorageHashMapKeyValue::<K, T>::serialized_size() as usize;
+        let data_size = (bytes.len() - SIZE_OFFSET) / value_offset;
+        let mut data = Vec::<StorageHashMapKeyValue<K, T>>::new();
+        data.reserve(data_size);
+
+        for i in 0..data_size {
+            let offset = SIZE_OFFSET + value_offset * i;
+            data.push(StorageHashMapKeyValue::<K, T>::deserialize(
+                &bytes[offset..],
+            )?);
+        }
+
+        Ok(StorageHashMapData { data, size })
     }
 
     fn serialize(&self) -> Vec<u8> {
-        let bytes = Vec<u8>::new();
-        bytes.reserve()
+        let mut bytes = Vec::<u8>::new();
+        let byte_size = std::mem::size_of::<u64>()
+            + self.data.len() * StorageHashMapKeyValue::<K, T>::serialized_size() as usize;
+
+        bytes.reserve(byte_size);
+        bytes.extend(self.size.serialize());
+
+        for record in &self.data {
+            bytes.extend(record.serialize());
+        }
+
+        bytes
     }
 }
 
@@ -43,7 +68,7 @@ mod tests {
             size: 2,
         };
         let bytes = data.serialize();
-        let other = StorageHashMapData::<i64, i64>::deserialize()?;
+        let other = StorageHashMapData::<i64, i64>::deserialize(&bytes).unwrap();
 
         assert_eq!(data, other);
     }

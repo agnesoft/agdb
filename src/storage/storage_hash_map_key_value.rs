99741a59da09203f911c638e353b555579366151
michaelvlach <vlach.michael@gmail.com>
2022-09-18 13:05:49 +0200

    Update storage_hash_map_key_value.rs

--- src/storage/storage_hash_map_key_value.rs
+++ src/storage/storage_hash_map_key_value.rs
@@ -2,14 +2,36 @@ use super::serialize::Serialize;
 use super::storage_hash_map_meta_value::MetaValue;
 use crate::DbError;
 
-#[derive(Debug, Default, PartialEq)]
-pub(crate) struct StorageHashMapKeyValue<K: Serialize, T: Serialize> {
+#[derive(Clone, Debug, Default, PartialEq)]
+pub(crate) struct StorageHashMapKeyValue<K, T>
+where
+    K: Clone + Default + Serialize,
+    T: Clone + Default + Serialize,
+{
     pub(crate) key: K,
     pub(crate) value: T,
     pub(crate) meta_value: MetaValue,
 }
 
-impl<K: Serialize, T: Serialize> Serialize for StorageHashMapKeyValue<K, T> {
+impl<K, T> StorageHashMapKeyValue<K, T>
+where
+    K: Clone + Default + Serialize,
+    T: Clone + Default + Serialize,
+{
+    pub(crate) fn serialized_size() -> u64 {
+        Self::meta_value_offset() + MetaValue::serialized_size()
+    }
+
+    pub(crate) fn meta_value_offset() -> u64 {
+        std::mem::size_of::<K>() as u64 + std::mem::size_of::<T>() as u64
+    }
+}
+
+impl<K, T> Serialize for StorageHashMapKeyValue<K, T>
+where
+    K: Clone + Default + Serialize,
+    T: Clone + Default + Serialize,
+{
     fn deserialize(bytes: &[u8]) -> Result<Self, DbError> {
         Ok(Self {
             key: K::deserialize(&bytes[0..])?,
@@ -22,7 +44,7 @@ impl<K: Serialize, T: Serialize> Serialize for StorageHashMapKeyValue<K, T> {
 
     fn serialize(&self) -> Vec<u8> {
         let mut data = Vec::<u8>::new();
-        data.reserve(std::mem::size_of::<K>() + std::mem::size_of::<T>() + 1);
+        data.reserve(Self::serialized_size() as usize);
         data.append(&mut self.key.serialize());
         data.append(&mut self.value.serialize());
         data.append(&mut self.meta_value.serialize());
@@ -79,5 +101,10 @@ mod tests {
                 .description,
             "value out of bounds"
         );
+    }
+
+    #[test]
+    fn serialized_size() {
+        assert_eq!(StorageHashMapKeyValue::<i64, i64>::serialized_size(), 17);
     }
 }

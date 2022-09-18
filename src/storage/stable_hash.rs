c804bb9cfc4f5aea645cf67f234176973d940f65
michaelvlach <vlach.michael@gmail.com>
2022-09-18 08:58:58 +0200

    Create stable_hash.rs

---
+++ src/storage/stable_hash.rs
@@ -0,0 +1,30 @@
+pub(crate) trait StableHash {
+    fn stable_hash(&self) -> u64;
+}
+
+impl StableHash for i64 {
+    fn stable_hash(&self) -> u64 {
+        self.clone() as u64
+    }
+}
+
+impl StableHash for u64 {
+    fn stable_hash(&self) -> u64 {
+        self.clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn i64() {
+        assert_eq!(10_i64.stable_hash(), 10_u64);
+    }
+
+    #[test]
+    fn u64() {
+        assert_eq!(10_u64.stable_hash(), 10_u64);
+    }
+}

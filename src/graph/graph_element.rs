b86359438bccb7202036594083513a9fe533d841
michaelvlach <vlach.michael@gmail.com>
2022-09-20 07:26:58 +0200

    Create graph_element.rs

---
+++ src/graph/graph_element.rs
@@ -0,0 +1,12 @@
+#[allow(dead_code)]
+pub(crate) struct GraphElement {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let _element = GraphElement {};
+    }
+}

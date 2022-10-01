4a864e2912f736b733d6ca2d6900d43a7bac2d33
michaelvlach <vlach.michael@gmail.com>
2022-10-01 12:15:08 +0200

    Update hash_multi_map.rs

--- src/storage/hash_multi_map.rs
+++ src/storage/hash_multi_map.rs
@@ -92,6 +92,26 @@ mod tests {
     }
 
     #[test]
+    fn iter() {
+        let mut map = HashMultiMap::<i64, i64>::new();
+
+        map.insert(1, 10).unwrap();
+        map.insert(5, 15).unwrap();
+        map.insert(5, 15).unwrap();
+        map.insert(7, 20).unwrap();
+        map.insert(2, 30).unwrap();
+        map.insert(2, 50).unwrap();
+        map.insert(4, 13).unwrap();
+        map.remove_key(&7).unwrap();
+
+        let mut actual = map.iter().collect::<Vec<(i64, i64)>>();
+        actual.sort();
+        let expected: Vec<(i64, i64)> = vec![(1, 10), (2, 30), (2, 50), (4, 13), (5, 15), (5, 15)];
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
     fn remove_deleted_key() {
         let mut map = HashMultiMap::<i64, i64>::new();
 

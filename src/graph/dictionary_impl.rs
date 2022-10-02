cce1a8ccab6bfd2ccc66ecee02edb04043f88bcd
michaelvlach <vlach.michael@gmail.com>
2022-10-02 11:41:30 +0200

    Update dictionary_impl.rs

--- src/graph/dictionary_impl.rs
+++ src/graph/dictionary_impl.rs
@@ -2,11 +2,23 @@ use super::dictionary_data::DictionaryData;
 use crate::DbError;
 
 pub(crate) struct DictionaryImpl<Data: DictionaryData> {
-    data: Data,
+    pub(super) data: Data,
 }
 
 impl<Data: DictionaryData> DictionaryImpl<Data> {
-    pub(crate) fn insert<T>(&mut self, index: i64, value: T) -> Result<(), DbError> {
-        Ok(())
+    pub(crate) fn index<T>(&self, value: &T) -> Result<Option<i64>, DbError> {
+        todo!()
+    }
+
+    pub(crate) fn insert<T>(&mut self, index: i64, value: &T) -> Result<(), DbError> {
+        todo!()
+    }
+
+    pub(crate) fn remove(&mut self, index: i64) -> Result<(), DbError> {
+        todo!()
+    }
+
+    pub(crate) fn value<T>(&mut self, index: i64) -> Result<Option<T>, DbError> {
+        todo!()
     }
 }

ea9b621bfa5b61c7a489bfafe7e77fb08f9dfdaf
michaelvlach <vlach.michael@gmail.com>
2022-10-02 11:41:24 +0200

    Update dictionary.rs

--- src/graph/dictionary.rs
+++ src/graph/dictionary.rs
@@ -2,3 +2,28 @@ use super::dictionary_data_memory::DictionaryDataMemory;
 use super::dictionary_impl::DictionaryImpl;
 
 pub(crate) type Dictionary = DictionaryImpl<DictionaryDataMemory>;
+
+impl Dictionary {
+    pub(crate) fn new() -> Dictionary {
+        Dictionary {
+            data: DictionaryDataMemory {},
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn index() {}
+
+    #[test]
+    fn insert() {}
+
+    #[test]
+    fn remove() {}
+
+    #[test]
+    fn value() {}
+}

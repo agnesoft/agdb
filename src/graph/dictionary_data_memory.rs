e8e90ee4f4b0d8621e208b3064adce6a03464f29
michaelvlach <vlach.michael@gmail.com>
2022-10-02 11:41:27 +0200

    Update dictionary_data_memory.rs

--- src/graph/dictionary_data_memory.rs
+++ src/graph/dictionary_data_memory.rs
@@ -1,1 +1,13 @@
+use super::dictionary_data::DictionaryData;
+
 pub(crate) struct DictionaryDataMemory {}
+
+impl DictionaryData for DictionaryDataMemory {
+    fn commit(&mut self) -> Result<(), crate::DbError> {
+        todo!()
+    }
+
+    fn transaction(&mut self) {
+        todo!()
+    }
+}

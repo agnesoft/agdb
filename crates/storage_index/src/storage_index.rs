9b899652ba6ca63097eaddabd440f1192eeeef63
michaelvlach <vlach.michael@gmail.com>
2022-10-08 23:24:04 +0200

    Implement serialize

--- crates/storage_index/src/storage_index.rs
+++ crates/storage_index/src/storage_index.rs
@@ -1,5 +1,3 @@
-use std::mem::size_of;
-
 #[derive(Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
 pub struct StorageIndex {
     pub(crate) index: i64,
@@ -16,10 +14,6 @@ impl StorageIndex {
 
     pub fn is_valid(&self) -> bool {
         0 < self.index
-    }
-
-    pub fn serialized_size() -> u64 {
-        size_of::<i64>() as u64
     }
 
     pub fn value(&self) -> i64 {
--- crates/storage_index/src/storage_record.rs
+++ crates/storage_index/src/storage_record.rs
@@ -1,15 +1,8 @@
 use crate::StorageIndex;
-use std::mem::size_of;
 
 #[derive(Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
 pub struct StorageRecord {
     pub index: StorageIndex,
     pub position: u64,
     pub size: u64,
-}
-
-impl StorageRecord {
-    pub fn serialized_size() -> u64 {
-        StorageIndex::serialized_size() + (size_of::<u64>() as u64)
-    }
 }

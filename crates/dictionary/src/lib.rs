6a0f04b312087dfa898d5a276587a12f5ee26971
michaelvlach <vlach.michael@gmail.com>
2022-10-09 16:41:17 +0200

    Update lib.rs

--- crates/dictionary/src/lib.rs
+++ crates/dictionary/src/lib.rs
@@ -6,10 +6,14 @@ mod dictionary_data_storage;
 mod dictionary_data_storage_indexes;
 mod dictionary_default;
 mod dictionary_impl;
+mod dictionary_index;
+mod dictionary_index_from;
+mod dictionary_index_serialize;
 mod dictionary_value;
 mod dictionary_value_serialize;
 mod storage_dictionary;
 mod storage_dictionary_try_from;
 
 pub use dictionary::Dictionary;
+pub use dictionary_index::DictionaryIndex;
 pub use storage_dictionary::StorageDictionary;

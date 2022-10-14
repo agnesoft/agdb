820cc7fa18141b9f3ed101e51ae0a3af9eaa1eb8
michaelvlach <vlach.michael@gmail.com>
2022-10-14 07:27:51 +0200

    Update graph_search_from.rs

--- crates/graph_search/src/graph_search_from.rs
+++ crates/graph_search/src/graph_search_from.rs
@@ -1,8 +1,13 @@
 use crate::graph_search::GraphSearch;
+use agdb_bit_set::BitSet;
 use agdb_graph::Graph;
 
 impl<'a> From<&'a Graph> for GraphSearch<'a> {
     fn from(graph: &'a Graph) -> Self {
-        GraphSearch { graph }
+        GraphSearch {
+            graph,
+            visited: BitSet::new(),
+            result: vec![],
+        }
     }
 }

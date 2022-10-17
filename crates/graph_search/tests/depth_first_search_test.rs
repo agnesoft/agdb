965be24246e7f652fed147e7c67f64e90e5290a4
michaelvlach <vlach.michael@gmail.com>
2022-10-17 20:03:17 +0200

    Update depth_first_search_test.rs

--- crates/graph_search/tests/depth_first_search_test.rs
+++ crates/graph_search/tests/depth_first_search_test.rs
@@ -31,3 +31,26 @@ fn empty_graph() {
 
     assert_eq!(result, vec![]);
 }
+
+#[test]
+fn cyclic_graph() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+
+    let edge1 = graph.insert_edge(&node1, &node2).unwrap();
+    let edge2 = graph.insert_edge(&node1, &node2).unwrap();
+    let edge3 = graph.insert_edge(&node2, &node3).unwrap();
+    let edge4 = graph.insert_edge(&node2, &node3).unwrap();
+    let edge5 = graph.insert_edge(&node3, &node1).unwrap();
+    let edge6 = graph.insert_edge(&node3, &node1).unwrap();
+
+    let result = GraphSearch::from(&graph).depth_first_search(&node1, &Handler::default());
+
+    assert_eq!(
+        result,
+        vec![node1, edge2, node2, edge4, node3, edge6, edge1, edge3, edge5]
+    );
+}

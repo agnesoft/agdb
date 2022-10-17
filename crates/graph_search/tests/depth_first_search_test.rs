82f5447166dc6a7b026036f1277f191fc9894e24
michaelvlach <vlach.michael@gmail.com>
2022-10-17 20:15:22 +0200

    Update depth_first_search_test.rs

--- crates/graph_search/tests/depth_first_search_test.rs
+++ crates/graph_search/tests/depth_first_search_test.rs
@@ -51,6 +51,135 @@ fn cyclic_graph() {
 
     assert_eq!(
         result,
-        vec![node1, edge2, node2, edge4, node3, edge6, edge1, edge3, edge5]
+        vec![node1, edge1, node2, edge3, node3, edge5, edge6, edge4, edge2]
     );
+}
+
+#[test]
+fn full_search() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+    let node4 = graph.insert_node().unwrap();
+
+    let edge1 = graph.insert_edge(&node1, &node2).unwrap();
+    let edge2 = graph.insert_edge(&node1, &node3).unwrap();
+    let edge3 = graph.insert_edge(&node1, &node4).unwrap();
+
+    let result = GraphSearch::from(&graph).depth_first_search(&node1, &Handler::default());
+
+    assert_eq!(
+        result,
+        vec![node1, edge1, node2, edge2, node3, edge3, node4]
+    );
+}
+
+#[test]
+fn filter_edges() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+    let node4 = graph.insert_node().unwrap();
+
+    graph.insert_edge(&node1, &node2).unwrap();
+    graph.insert_edge(&node1, &node3).unwrap();
+    graph.insert_edge(&node1, &node4).unwrap();
+
+    let result = GraphSearch::from(&graph).depth_first_search(
+        &node1,
+        &Handler {
+            processor: |index: &GraphIndex, _distance: &u64| {
+                SearchControl::Continue(index.is_node())
+            },
+        },
+    );
+
+    assert_eq!(result, vec![node1, node2, node3, node4]);
+}
+
+#[test]
+fn finish_search() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+
+    graph.insert_edge(&node1, &node2).unwrap();
+    graph.insert_edge(&node1, &node2).unwrap();
+    graph.insert_edge(&node2, &node3).unwrap();
+    graph.insert_edge(&node2, &node3).unwrap();
+    graph.insert_edge(&node3, &node1).unwrap();
+    graph.insert_edge(&node3, &node1).unwrap();
+
+    let result = GraphSearch::from(&graph).depth_first_search(
+        &node1,
+        &Handler {
+            processor: |index: &GraphIndex, _distance: &u64| {
+                if index.value() == 2 {
+                    SearchControl::Finish(true)
+                } else {
+                    SearchControl::Continue(false)
+                }
+            },
+        },
+    );
+
+    assert_eq!(result, vec![node2]);
+}
+
+#[test]
+fn search_twice() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+    let node4 = graph.insert_node().unwrap();
+
+    let edge1 = graph.insert_edge(&node1, &node2).unwrap();
+    let edge2 = graph.insert_edge(&node1, &node3).unwrap();
+    let edge3 = graph.insert_edge(&node1, &node4).unwrap();
+
+    let mut result = GraphSearch::from(&graph).depth_first_search(&node1, &Handler::default());
+    let expected = vec![node1.clone(), edge1, node2, edge2, node3, edge3, node4];
+
+    assert_eq!(result, expected);
+
+    result = GraphSearch::from(&graph).depth_first_search(&node1, &Handler::default());
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn stop_at_distance() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+
+    let edge1 = graph.insert_edge(&node1, &node2).unwrap();
+    let edge2 = graph.insert_edge(&node1, &node2).unwrap();
+    let _edge3 = graph.insert_edge(&node2, &node3).unwrap();
+    let _edge4 = graph.insert_edge(&node3, &node1).unwrap();
+
+    let result = GraphSearch::from(&graph).depth_first_search(
+        &node1,
+        &Handler {
+            processor: |_index: &GraphIndex, distance: &u64| {
+                if *distance == 2 {
+                    SearchControl::Stop(true)
+                } else {
+                    SearchControl::Continue(true)
+                }
+            },
+        },
+    );
+
+    assert_eq!(result, vec![node1, edge1, node2, edge2]);
 }

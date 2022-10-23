e92ed7594d338e4dfbdabc701d6bb3bf45c3d268
michaelvlach <vlach.michael@gmail.com>
2022-10-23 21:41:04 +0200

    Update path_search_test.rs

--- crates/graph_search/tests/path_search_test.rs
+++ crates/graph_search/tests/path_search_test.rs
@@ -22,6 +22,17 @@ impl PathSearchHandler for Handler {
 }
 
 #[test]
+fn circular_path() {
+    let mut graph = Graph::new();
+    let node = graph.insert_node().unwrap();
+    let _edge = graph.insert_edge(&node, &node).unwrap();
+
+    let result = GraphSearch::from(&graph).path(&node, &node, &Handler::default());
+
+    assert_eq!(result, vec![]);
+}
+
+#[test]
 fn empty_graph() {
     let graph = Graph::new();
 
@@ -30,6 +41,112 @@ fn empty_graph() {
         &GraphIndex::default(),
         &Handler::default(),
     );
+
+    assert_eq!(result, vec![]);
+}
+
+#[test]
+fn multi_edge_path() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+
+    let edge1 = graph.insert_edge(&node1, &node2).unwrap();
+    let _edge2 = graph.insert_edge(&node1, &node2).unwrap();
+
+    let edge3 = graph.insert_edge(&node2, &node3).unwrap();
+    let _edge4 = graph.insert_edge(&node2, &node3).unwrap();
+
+    let result = GraphSearch::from(&graph).path(&node1, &node3, &Handler::default());
+
+    assert_eq!(result, vec![node1, edge1, node2, edge3, node3]);
+}
+
+#[test]
+fn origin_is_destination() {
+    let mut graph = Graph::new();
+    let node = graph.insert_node().unwrap();
+
+    let result = GraphSearch::from(&graph).path(&node, &node, &Handler::default());
+
+    assert_eq!(result, vec![]);
+}
+
+#[test]
+fn short_circuit_path() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+
+    let edge1 = graph.insert_edge(&node1, &node3).unwrap();
+    let _edge2 = graph.insert_edge(&node1, &node2).unwrap();
+    let _edge3 = graph.insert_edge(&node2, &node3).unwrap();
+
+    let result = GraphSearch::from(&graph).path(&node1, &node3, &Handler::default());
+
+    assert_eq!(result, vec![node1, edge1, node3]);
+}
+
+#[test]
+fn single_path() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+
+    let edge1 = graph.insert_edge(&node1, &node2).unwrap();
+    let edge2 = graph.insert_edge(&node2, &node3).unwrap();
+
+    let result = GraphSearch::from(&graph).path(&node1, &node3, &Handler::default());
+
+    assert_eq!(result, vec![node1, edge1, node2, edge2, node3]);
+}
+
+#[test]
+fn skip_short_circuit_path() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+
+    let _edge1 = graph.insert_edge(&node1, &node3).unwrap();
+    let edge2 = graph.insert_edge(&node1, &node2).unwrap();
+    let edge3 = graph.insert_edge(&node2, &node3).unwrap();
+
+    let result = GraphSearch::from(&graph).path(
+        &node1,
+        &node3,
+        &Handler {
+            processor: |index: &GraphIndex, _distance: &u64| {
+                if index.value() == -4 {
+                    return 0;
+                }
+
+                1
+            },
+        },
+    );
+
+    assert_eq!(result, vec![node1, edge2, node2, edge3, node3]);
+}
+
+#[test]
+fn unconnected() {
+    let mut graph = Graph::new();
+
+    let node1 = graph.insert_node().unwrap();
+    let node2 = graph.insert_node().unwrap();
+    let node3 = graph.insert_node().unwrap();
+
+    let _edge1 = graph.insert_edge(&node1, &node2).unwrap();
+
+    let result = GraphSearch::from(&graph).path(&node1, &node3, &Handler::default());
 
     assert_eq!(result, vec![]);
 }

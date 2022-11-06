ead36f4d5572c8f5f151a47b6a455f12ef9ccde6
michaelvlach <vlach.michael@gmail.com>
2022-11-06 08:29:01 +0100

    Create db_float.rs

---
+++ src/agdb/db/db_float.rs
@@ -0,0 +1,121 @@
+use std::cmp::Ordering;
+use std::hash::Hash;
+
+#[derive(Clone, Debug)]
+pub struct DbFloat(f64);
+
+impl DbFloat {
+    pub fn as_f64(&self) -> f64 {
+        self.0
+    }
+
+    pub fn to_f64(self) -> f64 {
+        self.0
+    }
+}
+
+impl Eq for DbFloat {}
+
+impl Hash for DbFloat {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.to_bits().hash(state)
+    }
+}
+
+impl Ord for DbFloat {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.total_cmp(&other.0)
+    }
+}
+
+impl PartialEq for DbFloat {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.total_cmp(&other.0) == Ordering::Equal
+    }
+}
+
+impl PartialOrd for DbFloat {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.0.total_cmp(&other.0))
+    }
+}
+
+impl From<f32> for DbFloat {
+    fn from(value: f32) -> Self {
+        DbFloat { 0: value.into() }
+    }
+}
+
+impl From<f64> for DbFloat {
+    fn from(value: f64) -> Self {
+        DbFloat { 0: value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashSet;
+
+    #[test]
+    fn as_f64() {
+        let float = DbFloat::from(1.0_f64);
+        let _first = float.as_f64();
+        let _second = float.as_f64();
+    }
+
+    #[test]
+    fn derived_from_clone() {
+        let float = DbFloat::from(1.0_f64);
+        let _other = float.clone();
+    }
+
+    #[test]
+    fn derived_from_debug() {
+        format!("{:?}", DbFloat::from(1.0_f64));
+    }
+
+    #[test]
+    fn derived_from_eq() {
+        let float = DbFloat::from(1.0_f64);
+        let other = DbFloat::from(1.0_f64);
+
+        assert_eq!(float, other);
+    }
+
+    #[test]
+    fn derived_from_hash() {
+        let mut set = HashSet::<DbFloat>::new();
+        set.insert(DbFloat::from(1.0_f64));
+    }
+
+    #[test]
+    fn derived_from_ord() {
+        let mut vec = vec![
+            DbFloat::from(1.1_f64),
+            DbFloat::from(1.3_f64),
+            DbFloat::from(-3.333_f64),
+        ];
+        vec.sort();
+
+        assert_eq!(
+            vec,
+            vec![
+                DbFloat::from(-3.333_f64),
+                DbFloat::from(1.1_f64),
+                DbFloat::from(1.3_f64)
+            ]
+        );
+    }
+
+    #[test]
+    fn from() {
+        let _from_f32 = DbFloat::from(1.0_f32);
+        let _from_f64 = DbFloat::from(1.0_f64);
+    }
+
+    #[test]
+    fn to_f64() {
+        let _to_f64 = DbFloat::from(1.0_f64).to_f64();
+    }
+}

78283f6efcc3db27c48a26900911a158fa42255c
michaelvlach <vlach.michael@gmail.com>
2022-11-18 12:29:42 +0100

    Create file_storage.rs

---
+++ src/agdb/storage/file_storage.rs
@@ -0,0 +1,27 @@
+use super::Storage;
+use crate::utilities::partial_serialize::PartialSerialize;
+use crate::utilities::serialize::Serialize;
+use crate::DbError;
+use crate::DbIndex;
+
+pub struct FileStorage {}
+
+impl FileStorage {
+    pub fn new(filename: &String) -> Result<FileStorage, DbError> {
+        Ok(FileStorage {})
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_utilities::test_file::TestFile;
+
+    #[test]
+    fn insert() {
+        let test_file = TestFile::new();
+        let mut storage = FileStorage::new(test_file.file_name()).unwrap();
+
+        //let index1 = storage.insert(&String::new()).unwrap();
+    }
+}

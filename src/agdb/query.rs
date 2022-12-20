4fcdcdec8cced9d2148b2c55180d2a4eb955bd14
michaelvlach <vlach.michael@gmail.com>
2022-12-20 12:56:48 +0100

    Update query.rs

--- src/agdb/query.rs
+++ src/agdb/query.rs
@@ -1,33 +1,38 @@
+mod insert_aliases_query;
 mod insert_edges_query;
 mod insert_nodes_query;
-mod insert_values_query;
+mod insert_query;
 mod query_error;
+mod query_id;
 mod query_ids;
 mod query_result;
 mod query_values;
 mod search_query;
-mod select_keys_query;
+mod select_query;
 
+use self::insert_aliases_query::InsertAliasQuery;
 use self::insert_edges_query::InsertEdgesQuery;
 use self::insert_nodes_query::InsertNodeQuery;
-use self::insert_values_query::InsertValuesQuery;
+use self::insert_query::InsertQuery;
 use self::query_ids::QueryIds;
 use self::query_values::QueryValues;
 use self::search_query::SearchQuery;
-use self::select_keys_query::SelectKeysQuery;
+use self::select_query::SelectQuery;
 pub use query_error::QueryError;
 pub use query_result::QueryResult;
 
 pub enum Query {
+    Insert(InsertQuery),
+    InsertAliases(InsertAliasQuery),
     InsertEdges(InsertEdgesQuery),
     InsertNodes(InsertNodeQuery),
-    InsertValues(InsertValuesQuery),
-    SelectAliases(QueryIds),
-    SelectElements(QueryIds),
-    SelectKeys(SelectKeysQuery),
-    SelectKeyCount(QueryIds),
-    SelectCount(SearchQuery),
+    Remove(SelectQuery),
     Search(SearchQuery),
+    Select(SelectQuery),
+    SelectAliases(QueryIds),
+    SelectCount(SearchQuery),
+    SelectKeyCount(QueryIds),
+    SelectKeys(QueryIds),
 }
 
 impl Default for Query {
@@ -35,6 +40,7 @@ impl Default for Query {
         Query::InsertNodes(InsertNodeQuery {
             count: 0,
             values: QueryValues::None,
+            aliases: vec![],
         })
     }
 }

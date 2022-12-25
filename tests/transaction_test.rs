9bd517135a90b3a63bd82fd36b9c18df68b657ed
michaelvlach <vlach.michael@gmail.com>
2022-12-25 23:10:26 +0100

    Update transaction_test.rs

--- tests/transaction_test.rs
+++ tests/transaction_test.rs
@@ -1,23 +1,15 @@
 use agdb::Db;
+use agdb::QueryBuilder;
 use agdb::QueryError;
 use agdb::QueryResult;
 use agdb::Transaction;
 
 #[test]
-fn transaction_is_public_type() {
-    let _transaction = Transaction::default();
-}
-
-#[test]
-fn create_transaction_from_db() {
-    let db = Db::default();
-    let _transaction: Transaction = db.transaction();
-}
-
-#[test]
 fn exec_takes_query_returns_query_result() {
     let db = Db::default();
-    let _transaction = db.transaction();
+    let transaction = db.transaction();
+    let query = QueryBuilder::insert().node().query();
+    let _result = transaction.exec(query);
 }
 
 #[test]

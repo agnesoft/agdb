5d7c79d3b5332d26c7a03e47157f624e0fd5748f
michaelvlach <vlach.michael@gmail.com>
2022-12-29 10:15:24 +0100

    add insert edge

---
+++ src/agdb/query_builder/insert_edge.rs
@@ -0,0 +1,14 @@
+use super::insert_edge_from::InsertEdgeFrom;
+use crate::query::insert_edges_query::InsertEdgesQuery;
+use crate::query::query_id::QueryId;
+use crate::query::query_ids::QueryIds;
+
+pub struct InsertEdge(pub InsertEdgesQuery);
+
+impl InsertEdge {
+    pub fn from(mut self, id: QueryId) -> InsertEdgeFrom {
+        self.0.from = QueryIds::Id(id);
+
+        InsertEdgeFrom(self.0)
+    }
+}

24b30bd1b1196b51395b4bee1950d5a8940cf7c3
michaelvlach <vlach.michael@gmail.com>
2023-02-08 21:55:28 +0100

    Update indexed_map.rs

--- src/agdb/collections/indexed_map.rs
+++ src/agdb/collections/indexed_map.rs
@@ -1,60 +1,36 @@
-use std::collections::hash_map::Iter;
-use std::collections::HashMap;
-use std::hash::Hash;
+pub mod indexed_map_impl;
 
-struct IndexedMap<K, T>
-where
-    K: PartialEq + Eq + Hash + Clone,
-    T: PartialEq + Eq + Hash + Clone,
-{
-    keys_to_values: HashMap<K, T>,
-    values_to_keys: HashMap<T, K>,
-}
+use self::indexed_map_impl::IndexedMapImpl;
+use super::map::map_data_memory::MapDataMemory;
+use super::map::map_impl::MapImpl;
+use super::map::multi_map_impl::MultiMapImpl;
+use crate::utilities::stable_hash::StableHash;
+use std::hash::Hash;
+use std::marker::PhantomData;
+
+pub type IndexedMap<K, T> = IndexedMapImpl<K, T, MapDataMemory<K, T>, MapDataMemory<T, K>>;
 
 #[allow(dead_code)]
 impl<K, T> IndexedMap<K, T>
 where
-    K: PartialEq + Eq + Hash + Clone,
-    T: PartialEq + Eq + Hash + Clone,
+    K: Clone + Default + Eq + Hash + PartialEq + StableHash,
+    T: Clone + Default + Eq + Hash + PartialEq + StableHash,
 {
-    pub fn insert(&mut self, key: &K, value: &T) {
-        self.keys_to_values.insert((*key).clone(), (*value).clone());
-        self.values_to_keys.insert((*value).clone(), (*key).clone());
-    }
-
-    pub fn iter(&self) -> Iter<K, T> {
-        self.keys_to_values.iter()
-    }
-
-    pub fn key(&self, value: &T) -> Option<&K> {
-        self.values_to_keys.get(value)
-    }
-
     pub fn new() -> Self {
         Self {
-            keys_to_values: HashMap::<K, T>::new(),
-            values_to_keys: HashMap::<T, K>::new(),
+            keys_to_values: MapImpl {
+                multi_map: MultiMapImpl {
+                    data: MapDataMemory::<K, T>::new(),
+                    phantom_marker: PhantomData,
+                },
+            },
+            values_to_keys: MapImpl {
+                multi_map: MultiMapImpl {
+                    data: MapDataMemory::<T, K>::new(),
+                    phantom_marker: PhantomData,
+                },
+            },
         }
-    }
-
-    pub fn remove_key(&mut self, key: &K) {
-        if let Some(value) = self.keys_to_values.get(key) {
-            self.values_to_keys.remove(value);
-        }
-
-        self.keys_to_values.remove(key);
-    }
-
-    pub fn remove_value(&mut self, value: &T) {
-        if let Some(key) = self.values_to_keys.get(value) {
-            self.keys_to_values.remove(key);
-        }
-
-        self.values_to_keys.remove(value);
-    }
-
-    pub fn value(&self, key: &K) -> Option<&T> {
-        self.keys_to_values.get(key)
     }
 }
 
@@ -63,29 +39,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn new() {
-        let _ = IndexedMap::<u64, u64>::new();
-    }
-
-    #[test]
     fn insert() {
         let mut map = IndexedMap::<String, u64>::new();
         let key = "alias".to_string();
         let value = 1_u64;
-        map.insert(&key, &value);
 
-        assert_eq!(map.value(&key), Some(&value));
-        assert_eq!(map.key(&value), Some(&key));
+        assert_eq!(map.insert(&key, &value), Ok(()));
+
+        assert_eq!(map.value(&key), Ok(Some(value)));
+        assert_eq!(map.key(&value), Ok(Some(key)));
     }
 
     #[test]
     fn iter() {
         let mut map = IndexedMap::<String, u64>::new();
-        map.insert(&"alias1".to_string(), &1_u64);
-        map.insert(&"alias2".to_string(), &2_u64);
-        map.insert(&"alias3".to_string(), &3_u64);
+        assert_eq!(map.insert(&"alias1".to_string(), &1_u64), Ok(()));
+        assert_eq!(map.insert(&"alias2".to_string(), &2_u64), Ok(()));
+        assert_eq!(map.insert(&"alias3".to_string(), &3_u64), Ok(()));
 
-        let mut values = Vec::<(&String, &u64)>::new();
+        let mut values = Vec::<(String, u64)>::new();
 
         for key_value in map.iter() {
             values.push(key_value);
@@ -96,11 +68,41 @@ mod tests {
         assert_eq!(
             values,
             vec![
-                (&"alias1".to_string(), &1_u64),
-                (&"alias2".to_string(), &2_u64),
-                (&"alias3".to_string(), &3_u64)
+                ("alias1".to_string(), 1_u64),
+                ("alias2".to_string(), 2_u64),
+                ("alias3".to_string(), 3_u64)
             ]
         );
+    }
+
+    #[test]
+    fn replace_by_key() {
+        let mut map = IndexedMap::<String, u64>::new();
+        let key = "alias".to_string();
+        let value = 1_u64;
+        let new_value = 2_u64;
+
+        assert_eq!(map.insert(&key, &value), Ok(()));
+        assert_eq!(map.insert(&key, &new_value), Ok(()));
+
+        assert_eq!(map.value(&key), Ok(Some(new_value)));
+        assert_eq!(map.key(&new_value), Ok(Some(key)));
+        assert_eq!(map.key(&value), Ok(None));
+    }
+
+    #[test]
+    fn replace_by_value() {
+        let mut map = IndexedMap::<String, u64>::new();
+        let key = "alias".to_string();
+        let new_key = "new_alias".to_string();
+        let value = 1_u64;
+
+        assert_eq!(map.insert(&key, &value), Ok(()));
+        assert_eq!(map.insert(&new_key, &value), Ok(()));
+
+        assert_eq!(map.value(&key), Ok(None));
+        assert_eq!(map.value(&new_key), Ok(Some(value)));
+        assert_eq!(map.key(&value), Ok(Some(new_key)));
     }
 
     #[test]
@@ -108,15 +110,17 @@ mod tests {
         let mut map = IndexedMap::<String, u64>::new();
         let key = "alias".to_string();
         let value = 1_u64;
-        map.insert(&key, &value);
 
-        assert_eq!(map.value(&key), Some(&value));
-        assert_eq!(map.key(&value), Some(&key));
+        assert_eq!(map.insert(&key, &value), Ok(()));
 
-        map.remove_key(&key);
+        assert_eq!(map.value(&key), Ok(Some(value)));
+        assert_eq!(map.key(&value), Ok(Some(key.clone())));
 
-        assert_eq!(map.value(&key), None);
-        assert_eq!(map.key(&value), None);
+        map.remove_key(&key).unwrap();
+        map.remove_key(&key).unwrap();
+
+        assert_eq!(map.value(&key), Ok(None));
+        assert_eq!(map.key(&value), Ok(None));
     }
 
     #[test]
@@ -124,14 +128,16 @@ mod tests {
         let mut map = IndexedMap::<String, u64>::new();
         let key = "alias".to_string();
         let value = 1_u64;
-        map.insert(&key, &value);
 
-        assert_eq!(map.value(&key), Some(&value));
-        assert_eq!(map.key(&value), Some(&key));
+        assert_eq!(map.insert(&key, &value), Ok(()));
 
-        map.remove_value(&value);
+        assert_eq!(map.value(&key), Ok(Some(value)));
+        assert_eq!(map.key(&value), Ok(Some(key.clone())));
 
-        assert_eq!(map.value(&key), None);
-        assert_eq!(map.key(&value), None);
+        map.remove_value(&value).unwrap();
+        map.remove_value(&value).unwrap();
+
+        assert_eq!(map.value(&key), Ok(None));
+        assert_eq!(map.key(&value), Ok(None));
     }
 }

cb4e29ba490a70c31914e6479d9692180e7fbf60
michaelvlach <vlach.michael@gmail.com>
2023-02-08 21:55:26 +0100

    Update collections.rs

--- src/agdb/collections.rs
+++ src/agdb/collections.rs
@@ -2,6 +2,7 @@ pub mod bit_set;
 pub mod dictionary;
 pub mod dictionary_storage;
 pub mod indexed_map;
+pub mod indexed_map_storage;
 pub mod map_storage;
 pub mod multi_map;
 pub mod multi_map_storage;

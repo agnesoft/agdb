e0d36f454fcb4a67b7123668430820e8a6b4d8aa
michaelvlach <vlach.michael@gmail.com>
2023-04-24 22:05:20 +0200

    Delete insert_aliases_ids.rs

--- src/agdb/query_builder/insert_aliases_ids.rs
+++
@@ -1,9 +0,0 @@
-use crate::query::insert_aliases_query::InsertAliasesQuery;
-
-pub struct InsertAliasesIds(pub InsertAliasesQuery);
-
-impl InsertAliasesIds {
-    pub fn query(self) -> InsertAliasesQuery {
-        self.0
-    }
-}

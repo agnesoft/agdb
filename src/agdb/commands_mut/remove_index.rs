38d0d14b51f27ac50f33f47a7c29937e6d4d5cf3
michaelvlach <vlach.michael@gmail.com>
2023-04-30 16:24:02 +0200

    Update remove_index.rs

--- src/agdb/commands_mut/remove_index.rs
+++ src/agdb/commands_mut/remove_index.rs
@@ -2,25 +2,30 @@ use super::insert_index::InsertIndex;
 use super::CommandsMut;
 use crate::db::db_context::Context;
 use crate::Db;
+use crate::DbId;
 use crate::QueryError;
 use crate::QueryResult;
 
 #[derive(Debug, PartialEq)]
-pub struct RemoveIndex {}
+pub struct RemoveIndex {
+    pub(crate) id: Option<DbId>,
+}
 
 impl RemoveIndex {
     pub(crate) fn process(
         &self,
         db: &mut Db,
         result: &mut QueryResult,
-        context: &Context,
+        context: &mut Context,
     ) -> Result<CommandsMut, QueryError> {
-        if let Some(graph_index) = db.indexes.value(&context.id)? {
-            db.indexes.remove_key(&context.id)?;
-            result.result -= 1;
+        let id = self.id.unwrap_or(context.id);
 
+        if let Some(graph_index) = db.indexes.value(&id)? {
+            context.graph_index = graph_index;
+            db.indexes.remove_key(&id)?;
+            result.result -= 1;
             Ok(CommandsMut::InsertIndex(InsertIndex {
-                id: Some(context.id),
+                id: Some(id),
                 graph_index: Some(graph_index),
             }))
         } else {
@@ -35,11 +40,11 @@ mod tests {
 
     #[test]
     fn derived_from_debug() {
-        format!("{:?}", RemoveIndex {});
+        format!("{:?}", RemoveIndex { id: None });
     }
 
     #[test]
     fn derived_from_partial_eq() {
-        assert_eq!(RemoveIndex {}, RemoveIndex {});
+        assert_eq!(RemoveIndex { id: None }, RemoveIndex { id: None });
     }
 }

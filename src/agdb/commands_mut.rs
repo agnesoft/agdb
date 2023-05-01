1bd8d9361a8ec900d8e34203e45bed1c23fd618b
michaelvlach <vlach.michael@gmail.com>
2023-05-01 14:50:46 +0200

    Update commands_mut.rs

--- src/agdb/commands_mut.rs
+++ src/agdb/commands_mut.rs
@@ -1,6 +1,5 @@
 pub mod insert_alias;
 pub mod insert_edge;
-pub mod insert_index;
 pub mod insert_node;
 pub mod remove_alias;
 pub mod remove_edge;
@@ -9,7 +8,6 @@ pub mod remove_node;
 
 use self::insert_alias::InsertAlias;
 use self::insert_edge::InsertEdge;
-use self::insert_index::InsertIndex;
 use self::insert_node::InsertNode;
 use self::remove_alias::RemoveAlias;
 use self::remove_edge::RemoveEdge;
@@ -20,7 +18,6 @@ use self::remove_node::RemoveNode;
 pub enum CommandsMut {
     InsertAlias(InsertAlias),
     InsertEdge(InsertEdge),
-    InsertIndex(InsertIndex),
     InsertNode(InsertNode),
     RemoveAlias(RemoveAlias),
     RemoveEdge(RemoveEdge),

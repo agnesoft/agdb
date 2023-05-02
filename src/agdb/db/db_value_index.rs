788fe8e3362c5a5ddc0e040ddd01848e624785fb
Michael Vlach <vlach.michael@gmail.com>
2023-05-02 19:55:59 +0200

    [db] Add db value index #503 (#504)
    * Create db_value_index.rs
    
    * Update db.rs

---
+++ src/agdb/db/db_value_index.rs
@@ -0,0 +1,192 @@
+use super::db_error::DbError;
+use crate::utilities::serialize::Serialize;
+use crate::utilities::serialize_static::SerializeStatic;
+use std::mem::size_of;
+
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
+pub(crate) struct DbValueIndex {
+    pub(crate) value: [u8; 16],
+}
+
+#[allow(dead_code)]
+impl DbValueIndex {
+    pub(crate) fn new() -> Self {
+        Self { value: [0_u8; 16] }
+    }
+
+    pub(crate) fn get_type(&self) -> u8 {
+        self.value[15] >> 4
+    }
+
+    pub(crate) fn index(&self) -> u64 {
+        let mut bytes = [0_u8; 8];
+        bytes.copy_from_slice(&self.value[0..size_of::<u64>()]);
+        u64::from_le_bytes(bytes)
+    }
+
+    pub(crate) fn is_value(&self) -> bool {
+        self.size() != 0
+    }
+
+    pub(crate) fn set_type(&mut self, value: u8) {
+        let v = (value << 4) | self.size();
+        self.value[15] = v;
+    }
+
+    pub(crate) fn set_index(&mut self, index: u64) {
+        self.set_size(0);
+        self.value[0..size_of::<u64>()].copy_from_slice(&index.to_le_bytes());
+    }
+
+    pub(crate) fn set_value(&mut self, value: &[u8]) -> bool {
+        if value.len() > 15 {
+            return false;
+        }
+
+        self.set_size(value.len() as u8);
+        self.value[0..(value.len())].copy_from_slice(value);
+
+        true
+    }
+
+    pub(crate) fn size(&self) -> u8 {
+        self.value[15] & 0b00001111
+    }
+
+    pub(crate) fn value(&self) -> &[u8] {
+        let pos = self.size();
+        &self.value[0..(pos as usize)]
+    }
+
+    fn set_size(&mut self, size: u8) {
+        let v = (size & 0b00001111) | (self.value[0] & 0b11110000);
+        self.value[15] = v;
+    }
+}
+
+impl Serialize for DbValueIndex {
+    fn serialize(&self) -> Vec<u8> {
+        self.value.to_vec()
+    }
+
+    fn deserialize(bytes: &[u8]) -> Result<Self, DbError> {
+        if bytes.len() != 16 {
+            return Err(DbError::from(
+                "DbValueIndex deserialization error: out of bounds",
+            ));
+        }
+
+        let mut index = Self::default();
+        index.value.copy_from_slice(bytes);
+        Ok(index)
+    }
+
+    fn serialized_size(&self) -> u64 {
+        16
+    }
+}
+
+impl SerializeStatic for DbValueIndex {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::Hash;
+
+    #[test]
+    fn derived_from_hash() {
+        let mut hasher = DefaultHasher::new();
+        DbValueIndex::new().hash(&mut hasher);
+    }
+
+    #[test]
+    fn derived_from_debug() {
+        format!("{:?}", DbValueIndex::new());
+    }
+
+    #[test]
+    fn derived_from_clone_partial_eq() {
+        let index = DbValueIndex::new();
+        assert_eq!(index, index.clone());
+    }
+
+    #[test]
+    fn value_type() {
+        let mut index = DbValueIndex::default();
+        index.set_type(3);
+
+        assert_eq!(index.get_type(), 3);
+    }
+
+    #[test]
+    fn value() {
+        let mut index = DbValueIndex::default();
+        let value = vec![1_u8, 2_u8, 3_u8];
+
+        assert!(index.set_value(&value));
+        assert_eq!(index.value(), value);
+        assert_eq!(index.size(), 3);
+        assert!(index.is_value());
+    }
+
+    #[test]
+    fn value_max() {
+        let mut index = DbValueIndex::default();
+        let value = vec![1_u8; 15];
+        index.set_value(&value);
+
+        assert_eq!(index.value(), value);
+        assert_eq!(index.size(), 15);
+        assert!(index.is_value());
+    }
+
+    #[test]
+    fn value_too_large() {
+        let mut index = DbValueIndex::default();
+        let value = vec![1_u8; 16];
+        index.set_value(&value);
+
+        assert_eq!(index.value(), vec![]);
+        assert_eq!(index.size(), 0);
+        assert!(!index.is_value());
+    }
+
+    #[test]
+    fn set_index() {
+        let mut index = DbValueIndex::default();
+        index.set_index(10_u64);
+
+        assert_eq!(index.value(), vec![]);
+        assert_eq!(index.size(), 0);
+        assert_eq!(index.index(), 10_u64);
+        assert!(!index.is_value());
+    }
+
+    #[test]
+    fn serialize() {
+        let mut index = DbValueIndex::new();
+        index.set_value(&[1_u8, 2_u8, 3_u8]);
+
+        let data = index.serialize();
+        let other_index = DbValueIndex::deserialize(&data).unwrap();
+
+        assert_eq!(index, other_index);
+    }
+
+    #[test]
+    fn bad_deserialize() {
+        let bad_data = vec![1_u8; 15];
+        assert_eq!(
+            DbValueIndex::deserialize(&bad_data)
+                .unwrap_err()
+                .description,
+            "DbValueIndex deserialization error: out of bounds"
+        );
+    }
+
+    #[test]
+    fn serialized_size() {
+        assert_eq!(DbValueIndex::default().serialized_size(), 16);
+    }
+}

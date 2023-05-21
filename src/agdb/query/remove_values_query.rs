0fbc0d9d0d05bd8b62b6a98cae72663ab2cc62b6
Michael Vlach <vlach.michael@gmail.com>
2023-05-21 10:25:17 +0200

    [db] Implement remove values #532 (#533)
    * Update db.rs
    
    * Update remove_values_query.rs
    
    * Update remove.rs
    
    * Update remove_values.rs
    
    * Update remove_value_test.rs

--- src/agdb/query/remove_values_query.rs
+++ src/agdb/query/remove_values_query.rs
@@ -1,3 +1,23 @@
+use super::query_ids::QueryIds;
 use super::select_values_query::SelectValuesQuery;
+use crate::Db;
+use crate::QueryError;
+use crate::QueryMut;
+use crate::QueryResult;
 
 pub struct RemoveValuesQuery(pub SelectValuesQuery);
+
+impl QueryMut for RemoveValuesQuery {
+    fn process(&self, db: &mut Db, result: &mut QueryResult) -> Result<(), QueryError> {
+        match &self.0.ids {
+            QueryIds::Ids(ids) => {
+                for id in ids {
+                    let db_id = db.db_id(id)?;
+                    result.result -= db.remove_keys(db_id, &self.0.keys)?;
+                }
+                Ok(())
+            }
+            QueryIds::Search(_) => Err(QueryError::from("Invalid remove query")),
+        }
+    }
+}

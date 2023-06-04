2d1a3197cb57797f22d068919f6ecc32ed064836
michaelvlach <vlach.michael@gmail.com>
2023-06-04 10:18:32 +0200

    implement select key count

--- src/agdb/query/select_key_count_query.rs
+++ src/agdb/query/select_key_count_query.rs
@@ -1,3 +1,30 @@
 use super::query_ids::QueryIds;
+use crate::Db;
+use crate::DbElement;
+use crate::Query;
+use crate::QueryError;
+use crate::QueryResult;
 
 pub struct SelectKeyCountQuery(pub QueryIds);
+
+impl Query for SelectKeyCountQuery {
+    fn process(&self, db: &Db, result: &mut QueryResult) -> Result<(), QueryError> {
+        match &self.0 {
+            QueryIds::Ids(ids) => {
+                result.elements.reserve(ids.len());
+                result.result += ids.len() as i64;
+
+                for id in ids {
+                    let db_id = db.db_id(id)?;
+                    result.elements.push(DbElement {
+                        index: db_id,
+                        values: vec![("key_count", db.key_count(db_id)?).into()],
+                    });
+                }
+
+                Ok(())
+            }
+            QueryIds::Search(_) => Err(QueryError::from("Invalid select key count query")),
+        }
+    }
+}
--- tests/select_key_count_test.rs
+++ tests/select_key_count_test.rs
@@ -1,17 +1,67 @@
+mod test_db;
+
+use agdb::DbElement;
+use agdb::DbId;
 use agdb::QueryBuilder;
+use test_db::TestDb;
 
 #[test]
 fn select_key_count_ids() {
-    let _query = QueryBuilder::select()
-        .key_count()
-        .ids(&["alias".into()])
-        .query();
+    let mut db = TestDb::new();
+    db.exec_mut(
+        QueryBuilder::insert()
+            .nodes()
+            .aliases(&["alias".into()])
+            .values(&[&[
+                ("key", 100).into(),
+                (1, "value").into(),
+                (vec![1.1_f64], 1).into(),
+            ]])
+            .query(),
+        1,
+    );
+    db.exec_elements(
+        QueryBuilder::select()
+            .key_count()
+            .ids(&["alias".into()])
+            .query(),
+        &[DbElement {
+            index: DbId(1),
+            values: vec![("key_count", 3_u64).into()],
+        }],
+    );
 }
 
 #[test]
-fn select_key_count_search() {
-    let _query = QueryBuilder::select()
-        .key_count()
-        .search(QueryBuilder::search().from("alias".into()).query())
-        .query();
+fn select_keys_count_no_keys() {
+    let mut db = TestDb::new();
+    db.exec_mut(
+        QueryBuilder::insert()
+            .nodes()
+            .aliases(&["alias".into()])
+            .query(),
+        1,
+    );
+    db.exec_elements(
+        QueryBuilder::select()
+            .key_count()
+            .ids(&["alias".into()])
+            .query(),
+        &[DbElement {
+            index: DbId(1),
+            values: vec![("key_count", 0_u64).into()],
+        }],
+    );
+}
+
+#[test]
+fn select_keys_search() {
+    let db = TestDb::new();
+    db.exec_error(
+        QueryBuilder::select()
+            .key_count()
+            .search(QueryBuilder::search().from("alias".into()).query())
+            .query(),
+        "Invalid select key count query",
+    );
 }

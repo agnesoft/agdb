28ced664b5f69a353408dd59ee7e2e6e3b30db9f
michaelvlach <vlach.michael@gmail.com>
2023-06-04 09:54:14 +0200

    implement select keys

--- src/agdb/query/select_keys_query.rs
+++ src/agdb/query/select_keys_query.rs
@@ -1,3 +1,30 @@
 use super::query_ids::QueryIds;
+use crate::Db;
+use crate::DbElement;
+use crate::Query;
+use crate::QueryError;
+use crate::QueryResult;
 
 pub struct SelectKeysQuery(pub QueryIds);
+
+impl Query for SelectKeysQuery {
+    fn process(&self, db: &Db, result: &mut QueryResult) -> Result<(), QueryError> {
+        match &self.0 {
+            QueryIds::Ids(ids) => {
+                result.elements.reserve(ids.len());
+                result.result += ids.len() as i64;
+
+                for id in ids {
+                    let db_id = db.db_id(id)?;
+                    result.elements.push(DbElement {
+                        index: db_id,
+                        values: db.keys(db_id)?,
+                    });
+                }
+
+                Ok(())
+            }
+            QueryIds::Search(_) => Err(QueryError::from("Invalid select keys query")),
+        }
+    }
+}
--- tests/select_keys.rs
+++ tests/select_keys.rs
@@ -1,14 +1,66 @@
+mod test_db;
+
+use agdb::DbElement;
+use agdb::DbId;
+use agdb::DbValue;
 use agdb::QueryBuilder;
+use test_db::TestDb;
 
 #[test]
 fn select_keys_ids() {
-    let _query = QueryBuilder::select().keys().ids(&["alias".into()]).query();
+    let mut db = TestDb::new();
+    db.exec_mut(
+        QueryBuilder::insert()
+            .nodes()
+            .aliases(&["alias".into()])
+            .values(&[&[
+                ("key", 100).into(),
+                (1, "value").into(),
+                (vec![1.1_f64], 1).into(),
+            ]])
+            .query(),
+        1,
+    );
+    db.exec_elements(
+        QueryBuilder::select().keys().ids(&["alias".into()]).query(),
+        &[DbElement {
+            index: DbId(1),
+            values: vec![
+                ("key", DbValue::default()).into(),
+                (1, DbValue::default()).into(),
+                (vec![1.1_f64], DbValue::default()).into(),
+            ],
+        }],
+    );
+}
+
+#[test]
+fn select_keys_no_keys() {
+    let mut db = TestDb::new();
+    db.exec_mut(
+        QueryBuilder::insert()
+            .nodes()
+            .aliases(&["alias".into()])
+            .query(),
+        1,
+    );
+    db.exec_elements(
+        QueryBuilder::select().keys().ids(&["alias".into()]).query(),
+        &[DbElement {
+            index: DbId(1),
+            values: vec![],
+        }],
+    );
 }
 
 #[test]
 fn select_keys_search() {
-    let _query = QueryBuilder::select()
-        .keys()
-        .search(QueryBuilder::search().from("alias".into()).query())
-        .query();
+    let db = TestDb::new();
+    db.exec_error(
+        QueryBuilder::select()
+            .keys()
+            .search(QueryBuilder::search().from("alias".into()).query())
+            .query(),
+        "Invalid select keys query",
+    );
 }

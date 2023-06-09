82b791630bdadb57836f4d67d50c681eef27e46c
michaelvlach <vlach.michael@gmail.com>
2023-06-09 07:24:36 +0200

    select from search

--- src/agdb/query/select_query.rs
+++ src/agdb/query/select_query.rs
@@ -9,22 +9,29 @@ pub struct SelectQuery(pub QueryIds);
 
 impl Query for SelectQuery {
     fn process(&self, db: &Db, result: &mut QueryResult) -> Result<(), QueryError> {
-        match &self.0 {
+        let db_ids = match &self.0 {
             QueryIds::Ids(ids) => {
-                result.elements.reserve(ids.len());
-                result.result = ids.len() as i64;
+                let mut db_ids = vec![];
+                db_ids.reserve(ids.len());
 
-                for id in ids {
-                    let db_id = db.db_id(id)?;
-
-                    result.elements.push(DbElement {
-                        id: db_id,
-                        values: db.values(db_id)?,
-                    });
+                for query_id in ids {
+                    db_ids.push(db.db_id(query_id)?);
                 }
-                Ok(())
+
+                db_ids
             }
-            QueryIds::Search(_) => Err(QueryError::from("Invalid select query")),
+            QueryIds::Search(search_query) => search_query.search(db)?,
+        };
+
+        result.elements.reserve(db_ids.len());
+        result.result = db_ids.len() as i64;
+
+        for db_id in db_ids {
+            result.elements.push(DbElement {
+                id: db_id,
+                values: db.values(db_id)?,
+            });
         }
+        Ok(())
     }
 }

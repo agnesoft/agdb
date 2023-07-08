2bbfa598d4646466f3acb1b1f85b53856d76f27f
michaelvlach <vlach.michael@gmail.com>
2023-07-08 20:13:09 +0200

    add select docs

--- src/agdb/query_builder/select.rs
+++ src/agdb/query_builder/select.rs
@@ -11,25 +11,40 @@ use crate::query::select_keys_query::SelectKeysQuery;
 use crate::query::select_query::SelectQuery;
 use crate::query::select_values_query::SelectValuesQuery;
 
+/// Select builder that lets you choose what
+/// data you want to select form the database.
 pub struct Select {}
 
 impl Select {
+    /// Select aliases. If no ids are given all aliases
+    /// in the database will be selected. Each element
+    /// of the result will have a property `String("alias")`
+    /// holding the alias. If `ids` are specified and any
+    /// of them does not have an alias an error will occur
+    /// when running such query.
     pub fn aliases(self) -> SelectAliases {
         SelectAliases(SelectAliasesQuery(QueryIds::Ids(vec![])))
     }
 
+    /// Select elements with `ids` with all properties (key-values).
+    /// All ids specified must exist in the database.
     pub fn ids<T: Into<QueryIds>>(self, ids: T) -> SelectIds {
         SelectIds(SelectQuery(ids.into()))
     }
 
+    /// Select keys only (values will be empty).
     pub fn keys(self) -> SelectKeys {
         SelectKeys(SelectKeysQuery(QueryIds::Ids(vec![0.into()])))
     }
 
+    /// Select number of keys. Each element of the result will have
+    /// a property `String("key_count")` with `i64` as the value.
     pub fn key_count(self) -> SelectKeyCount {
         SelectKeyCount(SelectKeyCountQuery(QueryIds::Ids(vec![0.into()])))
     }
 
+    /// Select elements with `ids` with only `keys` properties (key-values).
+    /// All ids specified must exist in the database.
     pub fn values<T: Into<QueryKeys>>(self, keys: T) -> SelectValues {
         SelectValues(SelectValuesQuery {
             keys: Into::<QueryKeys>::into(keys).0,

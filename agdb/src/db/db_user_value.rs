e29ff4e6564f343eac3a419dca7cb58062296845
michaelvlach <vlach.michael@gmail.com>
2023-08-12 10:45:35 +0200

    Update db_user_value.rs

--- agdb/src/db/db_user_value.rs
+++ agdb/src/db/db_user_value.rs
@@ -1,9 +1,12 @@
+use crate::DbElement;
 use crate::DbError;
+use crate::DbId;
 use crate::DbKey;
 use crate::DbKeyValue;
 
 pub trait DbUserValue: Sized {
+    fn db_id(&self) -> Option<DbId>;
     fn db_keys() -> Vec<DbKey>;
-    fn from_db_values(values: &[DbKeyValue]) -> Result<Self, DbError>;
+    fn from_db_element(element: &DbElement) -> Result<Self, DbError>;
     fn to_db_values(&self) -> Vec<DbKeyValue>;
 }

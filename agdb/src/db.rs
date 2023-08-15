a2d405b8656abe9efc64bd48e030c41279894043
michaelvlach <vlach.michael@gmail.com>
2023-08-15 21:12:56 +0200

    Update db.rs

--- agdb/src/db.rs
+++ agdb/src/db.rs
@@ -195,6 +195,12 @@ impl Db {
         }
     }
 
+    /// Flushes the underlying file and copies it
+    /// to `filename` path.
+    pub fn backup(&mut self, filename: &str) -> Result<(), DbError> {
+        self.storage.backup(filename)
+    }
+
     /// Executes immutable query:
     ///
     /// - Select elements

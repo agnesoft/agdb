5e8f83f898aa0df6bb103bc1762d12563804f6c3
michaelvlach <vlach.michael@gmail.com>
2023-08-24 07:33:55 +0200

    wip

--- agdb_benchmarks/src/main.rs
+++ agdb_benchmarks/src/main.rs
@@ -1,16 +1,37 @@
 use crate::database::Database;
+use agdb::DbId;
 use agdb::QueryBuilder;
+use agdb::UserValue;
 use bench_result::BenchResult;
 
 mod bench_error;
 mod bench_result;
 mod database;
 
-fn main() -> BenchResult<()> {
-    let db = Database::new()?;
+const USER_COUNT: u64 = 10000;
+
+#[derive(UserValue)]
+struct User {
+    name: String,
+    email: String,
+}
+
+fn setup_users(db: &mut Database) -> BenchResult<()> {
+    db.0.write()?
+        .exec_mut(&QueryBuilder::insert().nodes().aliases("users").query());
+
+    let mut user_values: Vec<User>;
 
     db.0.write()?
-        .exec_mut(&QueryBuilder::insert().nodes().count(1).query())?;
+        .exec_mut(&QueryBuilder::insert().nodes().values(&user_values).query())?;
+
+    Ok(())
+}
+
+fn main() -> BenchResult<()> {
+    let mut db = Database::new()?;
+
+    setup_users(&mut db)?;
 
     Ok(())
 }

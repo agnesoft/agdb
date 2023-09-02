34192fdedbac1ee88db936cd6cc3269e72f714d8
michaelvlach <vlach.michael@gmail.com>
2023-09-02 14:20:00 +0200

    use config file

---
+++ agdb_benchmarks/src/config.rs
@@ -0,0 +1,110 @@
+use crate::bench_result::BenchResult;
+use crate::BENCH_CONFIG_FILE;
+use num_format::Locale;
+use serde::Deserialize;
+use serde::Serialize;
+use std::fs::File;
+use std::path::Path;
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct PostWriters {
+    pub(crate) count: u64,
+    pub(crate) posts: u64,
+    pub(crate) delay_ms: u64,
+    pub(crate) title: String,
+    pub(crate) body: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct CommentWriters {
+    pub(crate) count: u64,
+    pub(crate) comments: u64,
+    pub(crate) delay_ms: u64,
+    pub(crate) body: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct PostReaders {
+    pub(crate) count: u64,
+    pub(crate) posts: u64,
+    pub(crate) reads_per_reader: u64,
+    pub(crate) delay_ms: u64,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct CommentReaders {
+    pub(crate) count: u64,
+    pub(crate) comments: u64,
+    pub(crate) reads_per_reader: u64,
+    pub(crate) delay_ms: u64,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct Config {
+    pub(crate) db_name: String,
+    pub(crate) locale: Locale,
+    pub(crate) padding: u64,
+    pub(crate) cell_padding: u64,
+    pub(crate) posters: PostWriters,
+    pub(crate) commenters: CommentWriters,
+    pub(crate) post_readers: PostReaders,
+    pub(crate) comment_readers: CommentReaders,
+}
+
+impl Config {
+    pub(crate) fn load_config() -> BenchResult<Self> {
+        let path = Path::new(BENCH_CONFIG_FILE);
+
+        if !path.exists() {
+            println!("Using default config (saved to '{BENCH_CONFIG_FILE}')");
+            let config = Self::default();
+            let file = File::create(path)?;
+            serde_yaml::to_writer(file, &config)?;
+            Ok(config)
+        } else {
+            println!("Using existing config from '{BENCH_CONFIG_FILE}'");
+            let file = File::open(path)?;
+            Ok(serde_yaml::from_reader(file)?)
+        }
+    }
+
+    pub(crate) fn user_count(&self) -> u64 {
+        self.posters.count + self.commenters.count
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            db_name: "bench_db.agdb".to_string(),
+            locale: Locale::cs,
+            padding: 20,
+            cell_padding: 10,
+            posters: PostWriters {
+                count: 10,
+                posts: 100,
+                delay_ms: 100,
+                title: "Title of the testing post".to_string(),
+                body: "Body of the testing post should be longer than the title".to_string(),
+            },
+            commenters: CommentWriters {
+                count: 10,
+                comments: 100,
+                delay_ms: 100,
+                body: "This is a testing comment of a post.".to_string(),
+            },
+            post_readers: PostReaders {
+                count: 100,
+                posts: 10,
+                delay_ms: 100,
+                reads_per_reader: 100,
+            },
+            comment_readers: CommentReaders {
+                count: 100,
+                comments: 10,
+                delay_ms: 100,
+                reads_per_reader: 100,
+            },
+        }
+    }
+}
--- agdb_benchmarks/src/main.rs
+++ agdb_benchmarks/src/main.rs
@@ -1,77 +1,70 @@
+use crate::config::Config;
 use crate::database::Database;
 use bench_result::BenchResult;
-use num_format::Locale;
-use std::time::Duration;
 
 mod bench_error;
 mod bench_result;
+mod config;
 mod database;
 mod readers;
 mod users;
 mod utilities;
 mod writers;
 
-pub(crate) const BENCH_DATABASE: &str = "db.agdb";
-pub(crate) const LOCALE: Locale = Locale::cs;
-pub(crate) const PADDING: usize = 30;
-pub(crate) const CELL_PADDING: usize = 15;
-
-pub(crate) const POST_WRITERS: u32 = 10;
-pub(crate) const POSTS_PER_WRITER: u32 = 100;
-pub(crate) const POST_TITLE: &str = "Title of the testing post";
-pub(crate) const POST_BODY: &str = "Body of the testing post should be longer than the title";
-
-pub(crate) const COMMENT_WRITERS: u32 = 10;
-pub(crate) const COMMENTS_PER_WRITER: u32 = 100;
-pub(crate) const COMMENT_BODY: &str = "This is a testing comment of a post.";
-
-pub(crate) const WRITE_DELAY: Duration = Duration::from_millis(10);
-
-pub(crate) const POST_READERS: u32 = 100;
-pub(crate) const POSTS_PER_READ: u32 = 10;
-pub(crate) const READS_PER_POST_READER: u32 = 100;
-
-pub(crate) const COMMENTS_READERS: u32 = 100;
-pub(crate) const COMMENTS_PER_READ: u32 = 10;
-pub(crate) const READS_PER_COMMENTS_READER: u32 = 100;
-
-pub(crate) const READ_DELAY: Duration = Duration::from_millis(10);
-pub(crate) const USER_COUNT: u32 = POST_WRITERS + COMMENT_WRITERS;
+pub(crate) const BENCH_CONFIG_FILE: &str = "agdb_benchmarks.yaml";
 
 #[tokio::main]
 async fn main() -> BenchResult<()> {
+    let config = Config::load_config()?;
+
     println!("Running agdb benchmark\n\n");
-    utilities::print_header();
+    utilities::print_header(&config);
 
-    let mut db = Database::new()?;
-    users::setup_users(&mut db)?;
+    let mut db = Database::new(&config)?;
+    users::setup_users(&mut db, &config)?;
 
-    let mut posters = writers::start_post_writers(&mut db)?;
-    let mut commenters = writers::start_comment_writers(&mut db)?;
-    let mut post_readers = readers::start_post_readers(&mut db)?;
-    let mut comment_readers = readers::start_comment_readers(&mut db)?;
+    let mut posters = writers::start_post_writers(&mut db, &config)?;
+    let mut commenters = writers::start_comment_writers(&mut db, &config)?;
+    let mut post_readers = readers::start_post_readers(&mut db, &config)?;
+    let mut comment_readers = readers::start_comment_readers(&mut db, &config)?;
 
     posters
-        .join_and_report(&format!(
-            "{POST_WRITERS} posters * {POSTS_PER_WRITER} posts"
-        ))
+        .join_and_report(
+            "Write posts",
+            config.posters.count,
+            config.posters.posts,
+            1,
+            &config,
+        )
         .await?;
     commenters
-        .join_and_report(&format!(
-            "{COMMENT_WRITERS} commenters * {COMMENTS_PER_WRITER} comments"
-        ))
+        .join_and_report(
+            "Write comments",
+            config.commenters.count,
+            config.commenters.comments,
+            1,
+            &config,
+        )
         .await?;
     post_readers
-        .join_and_report(&format!(
-            "{POST_READERS} post readers * {POSTS_PER_READ} * {READS_PER_POST_READER}"
-        ))
+        .join_and_report(
+            "Read posts",
+            config.post_readers.count,
+            config.post_readers.reads_per_reader,
+            config.post_readers.posts,
+            &config,
+        )
         .await?;
     comment_readers
-        .join_and_report(&format!(
-            "{COMMENTS_READERS} comment readers * {COMMENTS_PER_READ} * {READS_PER_COMMENTS_READER}"
-         ))
+        .join_and_report(
+            "Read comments",
+            config.comment_readers.count,
+            config.comment_readers.reads_per_reader,
+            config.comment_readers.comments,
+            &config,
+        )
         .await?;
 
     println!("---");
-    db.stat()
+    db.stat(&config)
 }

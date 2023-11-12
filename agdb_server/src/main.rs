6f93dd69d949edaa80a425d3c4f949f4872991c5
Michael Vlach <vlach.michael@gmail.com>
2023-11-12 18:57:43 +0100

    add test to agdb_server

--- agdb_server/src/main.rs
+++ agdb_server/src/main.rs
@@ -1,7 +1,15 @@
 use agdb::DbError;
 
 fn main() -> Result<(), DbError> {
-    let _db = agdb::Db::new("agdb_server.agdb")?;
-
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn main_test() {
+        assert_eq!(main(), Ok(()))
+    }
 }

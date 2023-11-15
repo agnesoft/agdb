f39aa7f29093486585e1208ae88890b954d5fb88
Michael Vlach <vlach.michael@gmail.com>
2023-11-15 21:14:49 +0100

    add shutdown

--- agdb_server/src/app.rs
+++ agdb_server/src/app.rs
@@ -1,12 +1,25 @@
+use axum::extract::State;
 use axum::routing;
 use axum::Router;
+use hyper::StatusCode;
+use tokio::sync::broadcast::Sender;
 
-pub(crate) fn app() -> Router {
-    Router::new().route("/", routing::get(root))
+pub(crate) fn app(shutdown_sender: Sender<()>) -> Router {
+    Router::new().route("/", routing::get(root)).route(
+        "/shutdown",
+        routing::get(shutdown).with_state(shutdown_sender),
+    )
 }
 
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+async fn shutdown(State(shutdown_sender): State<Sender<()>>) -> StatusCode {
+    match shutdown_sender.send(()) {
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
 }
 
 #[cfg(test)]
@@ -19,7 +32,7 @@ mod tests {
 
     #[tokio::test]
     async fn hello_world() -> anyhow::Result<()> {
-        let app = app();
+        let app = app(Sender::<()>::new(1));
         let request = Request::builder().uri("/").body(Body::empty())?;
         let response = app.oneshot(request).await?;
 
@@ -27,6 +40,29 @@ mod tests {
 
         let body = hyper::body::to_bytes(response.into_body()).await?;
         assert_eq!(&body[..], b"Hello, World!");
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn shutdown() -> anyhow::Result<()> {
+        let (shutdown_sender, _shutdown_receiver) = tokio::sync::broadcast::channel::<()>(1);
+        let app = app(shutdown_sender);
+        let request = Request::builder().uri("/shutdown").body(Body::empty())?;
+        let response = app.oneshot(request).await?;
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn bad_shutdown() -> anyhow::Result<()> {
+        let app = app(Sender::<()>::new(1));
+        let request = Request::builder().uri("/shutdown").body(Body::empty())?;
+        let response = app.oneshot(request).await?;
+
+        assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
+
         Ok(())
     }
 }

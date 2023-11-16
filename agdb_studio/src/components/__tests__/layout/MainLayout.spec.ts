95288d9c70b00db2d8c5b7748c8d264afd6a2fe2
janavlachova <65499282+janavlachova@users.noreply.github.com>
2023-11-16 18:32:21 +0100

    [studio] Add login screen #770 (#772)
    added login form

--- agdb_studio/src/components/__tests__/layout/MainLayout.spec.ts
+++ agdb_studio/src/components/__tests__/layout/MainLayout.spec.ts
@@ -4,8 +4,8 @@ import { mount } from '@vue/test-utils'
 import MainLayout from '../../layout/MainLayout.vue'
 
 describe('MainLayout', () => {
-  it('renders properly', () => {
-    const wrapper = mount(MainLayout, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Main layout')
-  })
+    it('renders properly', () => {
+        const wrapper = mount(MainLayout, { props: { msg: 'Hello Vitest' } })
+        expect(wrapper.text()).toContain('Main layout')
+    })
 })
--- agdb_studio/src/components/layout/MainLayout.vue
+++ agdb_studio/src/components/layout/MainLayout.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="greetings"><h1>Main layout</h1></div>
+    <div class="greetings"><h1>Main layout</h1></div>
 </template>
 
 <style lang="css" scoped></style>

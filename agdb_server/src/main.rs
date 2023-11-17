575f881b87eff3146a690c285cbc9866ca14da0c
Michael Vlach <vlach.michael@gmail.com>
2023-11-17 09:30:06 +0100

    [server] Handle ctrl c #765 (#778)
    add ctrl+c handler

--- agdb_server/src/main.rs
+++ agdb_server/src/main.rs
@@ -2,18 +2,26 @@ mod app;
 
 use axum::Server;
 use std::net::SocketAddr;
+use tokio::signal;
+use tokio::sync::broadcast::Receiver;
+
+async fn shutdown_signal(mut shutdown_shutdown: Receiver<()>) {
+    tokio::select! {
+        _ = signal::ctrl_c() => {},
+        _ = shutdown_shutdown.recv() => {}
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let (shutdown_sender, mut shutdown_receiver) = tokio::sync::broadcast::channel::<()>(1);
-    let app = app::app(shutdown_sender);
+    let (shutdown_sender, shutdown_receiver) = tokio::sync::broadcast::channel::<()>(1);
+    let app = app::app(shutdown_sender.clone());
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let shutdown = shutdown_signal(shutdown_receiver);
 
     Server::bind(&addr)
         .serve(app.into_make_service())
-        .with_graceful_shutdown(async {
-            let _ = shutdown_receiver.recv().await;
-        })
+        .with_graceful_shutdown(shutdown)
         .await?;
 
     Ok(())
--- agdb_server/tests/shutdown_test.rs
+++ agdb_server/tests/shutdown_test.rs
@@ -2,7 +2,7 @@ use assert_cmd::prelude::*;
 use std::process::Command;
 
 #[tokio::test]
-async fn shutdown() -> anyhow::Result<()> {
+async fn shutdown_endpoint() -> anyhow::Result<()> {
     let mut server = Command::cargo_bin("agdb_server")?.spawn()?;
     assert!(reqwest::get("http://127.0.0.1:3000/shutdown")
         .await?

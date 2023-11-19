3d7a8d188e441fb08d61707e8dcf3c51838b4c92
janavlachova <65499282+janavlachova@users.noreply.github.com>
2023-11-19 11:36:53 +0100

    [studio] Extend browser support #785 (#786)
    extended browser support

--- agdb_studio/vite.config.ts
+++ agdb_studio/vite.config.ts
@@ -1,16 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    build: {
+        target: ["es2015", "edge88", "firefox78", "chrome87", "safari12"],
+    },
+});

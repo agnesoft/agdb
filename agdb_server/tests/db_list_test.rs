f0590d03840f1b6b404aa9fd0018cce6229701c4
Michael Vlach <vlach.michael@gmail.com>
2023-12-09 19:07:49 +0100

    db list

--- agdb_server/tests/db_list_test.rs
+++ agdb_server/tests/db_list_test.rs
@@ -2,34 +2,48 @@ pub mod framework;
 
 use crate::framework::Db;
 use crate::framework::TestServer;
-use crate::framework::DB_ADD_URI;
 use crate::framework::DB_LIST_URI;
+use crate::framework::NO_TOKEN;
 
 #[tokio::test]
 async fn list() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let token = server.init_user("alice", "mypassword123").await?;
-    let bad = Some("bad".to_string());
-    let db1 = Db {
-        name: "db1".to_string(),
-        db_type: "memory".to_string(),
-    };
-    let db2 = Db {
-        name: "db2".to_string(),
-        db_type: "memory".to_string(),
-    };
-
-    assert_eq!(server.get::<()>(DB_LIST_URI, &bad).await?.0, 401); //unauthorized
+    let token = server.init_user("alice", "password123").await?;
+    server.init_db("my_db", "memory", &token).await?;
+    server.init_db("my_other_db", "memory", &token).await?;
+    let expected = vec![
+        Db {
+            name: "my_db".to_string(),
+            db_type: "memory".to_string(),
+        },
+        Db {
+            name: "my_other_db".to_string(),
+            db_type: "memory".to_string(),
+        },
+    ];
     let (status, list) = server.get::<Vec<Db>>(DB_LIST_URI, &token).await?;
-    assert_eq!(status, 200); //Ok
-    assert_eq!(list?, vec![]);
-    assert_eq!(server.post(DB_ADD_URI, &db1, &token).await?.0, 201); //created
-    assert_eq!(server.post(DB_ADD_URI, &db2, &token).await?.0, 201); //created
-    let (status, list) = server.get::<Vec<Db>>(DB_LIST_URI, &token).await?;
+    assert_eq!(status, 200);
     let mut list = list?;
     list.sort();
-    assert_eq!(status, 200); //Ok
-    assert_eq!(list, vec![db1, db2]);
+    assert_eq!(list, expected);
+    Ok(())
+}
 
+#[tokio::test]
+async fn list_empty() -> anyhow::Result<()> {
+    let mut server = TestServer::new().await?;
+    let token = server.init_user("alice", "password123").await?;
+    let (status, list) = server.get::<Vec<Db>>(DB_LIST_URI, &token).await?;
+    assert_eq!(status, 200);
+    assert_eq!(list?, vec![]);
+    Ok(())
+}
+
+#[tokio::test]
+async fn list_no_token() -> anyhow::Result<()> {
+    let server = TestServer::new().await?;
+    let (status, list) = server.get::<Vec<Db>>(DB_LIST_URI, NO_TOKEN).await?;
+    assert_eq!(status, 401);
+    assert!(list.is_err());
     Ok(())
 }

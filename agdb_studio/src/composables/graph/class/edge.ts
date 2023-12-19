ec85e86b20fc4a2f3538fd74a0b4e9ea836a11c9
Jana Vlachova <jana.vlach@gmail.com>
2023-12-19 18:00:49 +0100

    studio - redo the force directed graph as es6 class

---
+++ agdb_studio/src/composables/graph/class/edge.ts
@@ -0,0 +1,78 @@
+import Node from "./node";
+
+export type EdgeOptions = {
+    id: number;
+    from?: Node;
+    to?: Node;
+    values: { [key: string]: string };
+};
+
+export default class Edge {
+    private id: number;
+    private from: Node | undefined;
+    private to: Node | undefined;
+    private values: Map<string, string>;
+
+    constructor({ id, from, to, values }: EdgeOptions) {
+        this.from = from;
+        this.to = to;
+        this.values = new Map<string, string>(Object.entries(values));
+        this.id = id;
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public getLength(): number {
+        if (this.from === undefined || this.to === undefined) {
+            return 0;
+        }
+
+        return this.from.dist(this.to);
+    }
+
+    public getDx(): number {
+        if (this.from === undefined || this.to === undefined) {
+            return 0;
+        }
+
+        return this.to.getX() - this.from.getX();
+    }
+
+    public getDy(): number {
+        if (this.from === undefined || this.to === undefined) {
+            return 0;
+        }
+
+        return this.to.getY() - this.from.getY();
+    }
+
+    public getDz(): number {
+        if (this.from === undefined || this.to === undefined) {
+            return 0;
+        }
+
+        return this.to.getZ() - this.from.getZ();
+    }
+
+    public getValuesEntries(): IterableIterator<[string, string]> {
+        return this.values.entries();
+    }
+
+    public getFrom(): Node | undefined {
+        return this.from;
+    }
+
+    public setFrom(from: Node | undefined): void {
+        this.from = from;
+    }
+
+    public getTo(): Node | undefined {
+        return this.to;
+    }
+
+    public setTo(to: Node | undefined): void {
+        this.to = to;
+    }
+}
---
+++ agdb_studio/src/composables/graph/class/node.ts
@@ -0,0 +1,104 @@
+export type Coordinates = {
+    x: number;
+    y: number;
+    z: number;
+};
+
+type NodeOptions = {
+    id: number;
+    values: { [key: string]: string };
+    coordinates: Coordinates;
+};
+
+export default class Node {
+    private id: number;
+    private values: Map<string, string>;
+
+    private x: number;
+    private y: number;
+    private z: number;
+
+    private vx: number = 0;
+    private vy: number = 0;
+    private vz: number = 0;
+
+    constructor(options: NodeOptions) {
+        this.id = options.id;
+        this.x = options.coordinates.x;
+        this.y = options.coordinates.y;
+        this.z = options.coordinates.z;
+        this.values = new Map<string, string>(Object.entries(options.values));
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public getCoordinates(): Coordinates {
+        return { x: this.x, y: this.y, z: this.z };
+    }
+
+    public setCoordinates(x: number, y: number, z: number = 0): Node {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        return this;
+    }
+
+    public getValuesEntries(): IterableIterator<[string, string]> {
+        return this.values.entries();
+    }
+
+    public getVelocity(): Coordinates {
+        return { x: this.vx, y: this.vy, z: this.vz };
+    }
+
+    public getVelocityLength(): number {
+        return Math.sqrt(Math.pow(this.vx, 2) + Math.pow(this.vy, 2) + Math.pow(this.vz, 2));
+    }
+
+    public setVelocity(vx: number, vy: number, vz: number): Node {
+        this.vx = vx;
+        this.vy = vy;
+        this.vz = vz;
+        return this;
+    }
+
+    public move(damper: number): void {
+        this.x += this.vx * damper;
+        this.y += this.vy * damper;
+        this.z += this.vz * damper;
+        this.resetVelocity();
+    }
+
+    public resetVelocity(): void {
+        this.vx = 0;
+        this.vy = 0;
+        this.vz = 0;
+    }
+
+    public addVelocity(vx: number, vy: number, vz: number): Node {
+        this.vx += vx;
+        this.vy += vy;
+        this.vz += vz;
+        return this;
+    }
+
+    public getX(): number {
+        return this.x;
+    }
+
+    public getY(): number {
+        return this.y;
+    }
+
+    public getZ(): number {
+        return this.z;
+    }
+
+    public dist(v: Node): number {
+        return Math.sqrt(
+            Math.pow(this.x - v.x, 2) + Math.pow(this.y - v.y, 2) + Math.pow(this.z - v.z, 2),
+        );
+    }
+}

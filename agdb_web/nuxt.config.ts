70d9f2c11c1dcc7c6aa50ecbfea639b5b5a74f9f
Jana Vlachova <jana.vlach@gmail.com>
2023-12-30 16:35:48 +0100

    vitest added

--- agdb_web/nuxt.config.ts
+++ agdb_web/nuxt.config.ts
@@ -1,5 +1,17 @@
+import { fileURLToPath, URL } from "node:url";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["@nuxt/content"],
+    modules: ["@nuxt/content", "@nuxt/test-utils/module"],
+    vite: {
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL(".", import.meta.url)),
+            },
+        },
+        build: {
+            target: ["es2015", "edge88", "firefox78", "chrome87", "safari12"],
+        },
+    },
 });
---
+++ agdb_web/vitest.config.ts
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from "node:url";
+import { configDefaults, coverageConfigDefaults } from "vitest/config";
+import { defineVitestConfig } from "@nuxt/test-utils/config";
+
+export default defineVitestConfig({
+    test: {
+        environment: "nuxt",
+        exclude: [...configDefaults.exclude, "e2e/*"],
+        root: fileURLToPath(new URL("./", import.meta.url)),
+        coverage: {
+            all: true,
+            exclude: [
+                ...coverageConfigDefaults.exclude,
+                "e2e/*",
+                "*.config.ts",
+            ],
+        },
+    },
+    build: {
+        target: ["es2015", "edge88", "firefox78", "chrome87", "safari12"],
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL(".", import.meta.url)),
+        },
+    },
+});

1903f22c966140d071f29cfe2d61aad39b7dae62
Jana Vlachova <jana.vlach@gmail.com>
2023-12-31 12:05:49 +0100

    added playwright

--- agdb_web/pages/[...slug].vue
+++ agdb_web/pages/[...slug].vue
@@ -1,6 +1,12 @@
 <template>
     <main>
-        <img alt="agdb logo" class="logo" src="@/static/logo.svg" width="125" height="125" />
+        <img
+            alt="agdb logo"
+            class="logo"
+            src="@/static/logo.svg"
+            width="125"
+            height="125"
+        />
         <h1>agdb</h1>
         <ContentDoc />
     </main>

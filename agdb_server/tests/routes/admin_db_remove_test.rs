9e7bdf5600f167ad6e63dc271d0f20523100b9ba
Michael Vlach <vlach.michael@gmail.com>
2024-01-06 20:58:19 +0100

    admin_db_remove

--- agdb_server/tests/routes/admin_db_remove_test.rs
+++ agdb_server/tests/routes/admin_db_remove_test.rs
@@ -1,75 +1,90 @@
-use crate::Db;
 use crate::TestServer;
-use crate::DB_LIST_URI;
-use crate::NO_TOKEN;
+use crate::ADMIN;
 use agdb_api::DbType;
 use std::path::Path;
 
 #[tokio::test]
 async fn remove() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let db = server.init_db(DbType::Mapped, &user).await?;
-    assert_eq!(
-        server
-            .delete(&format!("/admin/db/{db}/remove"), &server.admin_token)
-            .await?,
-        204
-    );
-    let (status, list) = server.get::<Vec<Db>>(DB_LIST_URI, &user.token).await?;
-    assert_eq!(status, 200);
-    assert!(list?.is_empty());
-    assert!(Path::new(&server.data_dir).join(db).exists());
+    let owner = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.admin_db_add(owner, db, DbType::Mapped).await?;
+    assert!(Path::new(&server.data_dir).join(owner).join(db).exists());
+    let status = server.api.admin_db_remove(owner, db).await?;
+    assert!(!server
+        .api
+        .admin_db_list()
+        .await?
+        .1
+        .iter()
+        .any(|d| d.name == format!("{}/{}", owner, db)));
+    assert_eq!(status, 204);
+    assert!(Path::new(&server.data_dir).join(owner).join(db).exists());
     Ok(())
 }
 
 #[tokio::test]
 async fn db_not_found() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    assert_eq!(
-        server
-            .delete(
-                &format!("/admin/db/{}/not_found/remove", user.name),
-                &server.admin_token
-            )
-            .await?,
-        404
-    );
+    let owner = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    let status = server
+        .api
+        .admin_db_remove(owner, db)
+        .await
+        .unwrap_err()
+        .status;
+    assert_eq!(status, 404);
     Ok(())
 }
 
 #[tokio::test]
 async fn user_not_found() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    assert_eq!(
-        server
-            .delete("/admin/db/not_found/db/remove", &server.admin_token)
-            .await?,
-        404
-    );
+    let owner = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    let status = server
+        .api
+        .admin_db_remove(owner, db)
+        .await
+        .unwrap_err()
+        .status;
+    assert_eq!(status, 404);
     Ok(())
 }
 
 #[tokio::test]
 async fn non_admin() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    assert_eq!(
-        server
-            .delete("/admin/db/user/db/remove", &user.token)
-            .await?,
-        401
-    );
+    let owner = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.user_login(owner, owner).await?;
+    let status = server
+        .api
+        .admin_db_remove(owner, db)
+        .await
+        .unwrap_err()
+        .status;
+    assert_eq!(status, 401);
     Ok(())
 }
 
 #[tokio::test]
 async fn no_token() -> anyhow::Result<()> {
-    let mut server = TestServer::new().await?;
-    assert_eq!(
-        server.delete("/admin/db/user/db/remove", NO_TOKEN).await?,
-        401
-    );
+    let server = TestServer::new().await?;
+    let status = server
+        .api
+        .admin_db_remove("owner", "db")
+        .await
+        .unwrap_err()
+        .status;
+    assert_eq!(status, 401);
     Ok(())
 }

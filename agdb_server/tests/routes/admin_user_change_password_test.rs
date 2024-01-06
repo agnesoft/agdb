9a2ad14cfe78d52ff75980ba88f328f333dc9e7a
Michael Vlach <vlach.michael@gmail.com>
2024-01-06 22:30:13 +0100

    admin_user_change_password

--- agdb_server/tests/routes/admin_user_change_password_test.rs
+++ agdb_server/tests/routes/admin_user_change_password_test.rs
@@ -1,110 +1,78 @@
 use crate::TestServer;
-use crate::UserCredentials;
-use crate::UserLogin;
-use crate::NO_TOKEN;
+use crate::ADMIN;
 
 #[tokio::test]
 async fn change_password() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let credentials = Some(UserCredentials {
-        password: "password456",
-    });
-
-    assert_eq!(
-        server
-            .put(
-                &format!("/admin/user/{}/change_password", user.name),
-                &credentials,
-                &server.admin_token
-            )
-            .await?,
-        201
-    );
-
-    let login = Some(UserLogin {
-        password: "password456",
-        username: &user.name,
-    });
-
-    assert_eq!(server.post("/user/login", &login, NO_TOKEN).await?.0, 200);
+    let owner = &server.next_user_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    let status = server
+        .api
+        .admin_user_change_password(owner, "password123")
+        .await?;
+    assert_eq!(status, 201);
+    let status = server.api.user_login(owner, "password123").await?;
+    assert_eq!(status, 200);
     Ok(())
 }
 
 #[tokio::test]
 async fn password_too_short() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let credentials = Some(UserCredentials { password: "pswd" });
-
-    assert_eq!(
-        server
-            .put(
-                &format!("/admin/user/{}/change_password", user.name),
-                &credentials,
-                &server.admin_token
-            )
-            .await?,
-        461
-    );
+    let owner = &server.next_user_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    let status = server
+        .api
+        .admin_user_change_password(owner, "pswd")
+        .await
+        .unwrap_err()
+        .status;
+    assert_eq!(status, 461);
     Ok(())
 }
 
 #[tokio::test]
 async fn user_not_found() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let credentials = Some(UserCredentials {
-        password: "password456",
-    });
-
-    assert_eq!(
-        server
-            .put(
-                "/admin/user/not_found/change_password",
-                &credentials,
-                &server.admin_token
-            )
-            .await?,
-        404
-    );
+    server.api.user_login(ADMIN, ADMIN).await?;
+    let status = server
+        .api
+        .admin_user_change_password("owner", "password123")
+        .await
+        .unwrap_err()
+        .status;
+    assert_eq!(status, 404);
     Ok(())
 }
 
 #[tokio::test]
 async fn non_admin() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let credentials = Some(UserCredentials {
-        password: "password456",
-    });
-    assert_eq!(
-        server
-            .put(
-                "/admin/user/not_found/change_password",
-                &credentials,
-                &user.token
-            )
-            .await?,
-        401
-    );
+    let owner = &server.next_user_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.user_login(owner, owner).await?;
+    let status = server
+        .api
+        .admin_user_change_password(owner, "password123")
+        .await
+        .unwrap_err()
+        .status;
+    assert_eq!(status, 401);
     Ok(())
 }
 
 #[tokio::test]
 async fn no_token() -> anyhow::Result<()> {
-    let mut server = TestServer::new().await?;
-    let credentials = Some(UserCredentials {
-        password: "password456",
-    });
-    assert_eq!(
-        server
-            .put(
-                "/admin/user/not_found/change_password",
-                &credentials,
-                NO_TOKEN
-            )
-            .await?,
-        401
-    );
+    let server = TestServer::new().await?;
+    let status = server
+        .api
+        .admin_user_change_password("owner", "password123")
+        .await
+        .unwrap_err()
+        .status;
+    assert_eq!(status, 401);
     Ok(())
 }

01b49c88778e08b532483148bc8bcff7d3cdfd17
Michael Vlach <vlach.michael@gmail.com>
2024-01-07 10:41:50 +0100

    db_optimize

--- agdb_server/tests/routes/db_optimize_test.rs
+++ agdb_server/tests/routes/db_optimize_test.rs
@@ -1,79 +1,81 @@
-use crate::Db;
 use crate::TestServer;
-use crate::DB_LIST_URI;
-use crate::NO_TOKEN;
+use crate::ADMIN;
 use agdb::QueryBuilder;
-use agdb::QueryType;
 use agdb_api::DbType;
+use agdb_api::DbUserRole;
 
 #[tokio::test]
 async fn optimize() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let db = server.init_db(DbType::Mapped, &user).await?;
-    let queries: Option<Vec<QueryType>> = Some(vec![QueryBuilder::insert()
-        .nodes()
-        .count(100)
-        .query()
-        .into()]);
-    server
-        .post(&format!("/db/{db}/exec"), &queries, &user.token)
-        .await?;
-    let original_size = server.get::<Vec<Db>>(DB_LIST_URI, &user.token).await?.1?[0].size;
-    let (status, response) = server
-        .post::<()>(&format!("/db/{db}/optimize"), &None, &user.token)
-        .await?;
+    let owner = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.user_login(owner, owner).await?;
+    server.api.db_add(owner, db, DbType::Mapped).await?;
+    let queries = &vec![QueryBuilder::insert().nodes().count(100).query().into()];
+    server.api.db_exec(owner, db, queries).await?;
+    let original_size = server
+        .api
+        .db_list()
+        .await?
+        .1
+        .iter()
+        .find(|d| d.name == format!("{}/{}", owner, db))
+        .unwrap()
+        .size;
+    let (status, db) = server.api.db_optimize(owner, db).await?;
     assert_eq!(status, 200);
-    let optimized_size = serde_json::from_str::<Db>(&response)?.size;
-    assert!(optimized_size < original_size);
-
+    assert!(db.size < original_size);
     Ok(())
 }
 
 #[tokio::test]
 async fn permission_denied() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let other = server.init_user().await?;
-    let db = server.init_db(DbType::Mapped, &user).await?;
-    assert_eq!(
-        server
-            .put::<()>(
-                &format!("/db/{db}/user/{}/add?db_role=read", other.name),
-                &None,
-                &user.token
-            )
-            .await?,
-        201
-    );
-    let status = server
-        .post::<()>(&format!("/db/{db}/optimize"), &None, &other.token)
-        .await?
-        .0;
+    let owner = &server.next_user_name();
+    let user = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.admin_user_add(user, user).await?;
+    server.api.admin_db_add(owner, db, DbType::Mapped).await?;
+    server
+        .api
+        .admin_db_user_add(owner, db, user, DbUserRole::Read)
+        .await?;
+    server.api.user_login(user, user).await?;
+    let status = server.api.db_optimize(owner, db).await.unwrap_err().status;
     assert_eq!(status, 403);
-
     Ok(())
 }
 
 #[tokio::test]
 async fn db_not_found() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
+    let owner = &server.next_user_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.user_login(owner, owner).await?;
     let status = server
-        .post::<()>("/db/user/not_found/optimize", &None, &user.token)
-        .await?
-        .0;
+        .api
+        .db_optimize(owner, "db")
+        .await
+        .unwrap_err()
+        .status;
     assert_eq!(status, 404);
     Ok(())
 }
 
 #[tokio::test]
 async fn no_token() -> anyhow::Result<()> {
-    let mut server = TestServer::new().await?;
+    let server = TestServer::new().await?;
     let status = server
-        .post::<()>("/db/user/not_found/optimize", &None, NO_TOKEN)
-        .await?
-        .0;
+        .api
+        .db_optimize("owner", "db")
+        .await
+        .unwrap_err()
+        .status;
     assert_eq!(status, 401);
     Ok(())
 }

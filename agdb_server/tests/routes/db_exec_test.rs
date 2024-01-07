c8a89310baeaccc427ec83ce7d09f93e64377657
Michael Vlach <vlach.michael@gmail.com>
2024-01-07 10:06:00 +0100

    fix db_exec

--- agdb_server/tests/routes/db_exec_test.rs
+++ agdb_server/tests/routes/db_exec_test.rs
@@ -1,18 +1,22 @@
 use crate::TestServer;
-use crate::NO_TOKEN;
+use crate::ADMIN;
 use agdb::DbElement;
 use agdb::DbId;
 use agdb::QueryBuilder;
 use agdb::QueryResult;
-use agdb::QueryType;
 use agdb_api::DbType;
+use agdb_api::DbUserRole;
 
 #[tokio::test]
 async fn read_write() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let db = server.init_db(DbType::Memory, &user).await?;
-    let queries: Option<Vec<QueryType>> = Some(vec![
+    let owner = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.user_login(owner, owner).await?;
+    server.api.db_add(owner, db, DbType::Mapped).await?;
+    let queries = &vec![
         QueryBuilder::insert()
             .nodes()
             .aliases("root")
@@ -20,12 +24,9 @@ async fn read_write() -> anyhow::Result<()> {
             .query()
             .into(),
         QueryBuilder::select().ids("root").query().into(),
-    ]);
-    let (status, responses) = server
-        .post(&format!("/db/{db}/exec"), &queries, &user.token)
-        .await?;
+    ];
+    let (status, results) = server.api.db_exec(owner, db, queries).await?;
     assert_eq!(status, 200);
-    let responses: Vec<QueryResult> = serde_json::from_str(&responses)?;
     let expected = vec![
         QueryResult {
             result: 1,
@@ -46,41 +47,30 @@ async fn read_write() -> anyhow::Result<()> {
             }],
         },
     ];
-
-    assert_eq!(responses.len(), expected.len());
-    assert_eq!(responses[0].result, expected[0].result);
-    assert_eq!(responses[0].elements, expected[0].elements);
-    assert_eq!(responses[1].result, expected[1].result);
-    assert_eq!(responses[1].elements, expected[1].elements);
-
+    assert_eq!(results, expected);
     Ok(())
 }
 
 #[tokio::test]
 async fn read_only() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let db = server.init_db(DbType::Memory, &user).await?;
-    let queries: Option<Vec<QueryType>> = Some(vec![
-        // Wrap the queries vector with Some()
-        QueryBuilder::insert()
-            .nodes()
-            .aliases("root")
-            .values(vec![vec![("key", 1.1).into()]])
-            .query()
-            .into(),
-    ]);
-    let (status, _responses) = server
-        .post(&format!("/db/{db}/exec"), &queries, &user.token) // Pass queries as &queries instead of &queries
-        .await?;
+    let owner = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.user_login(owner, owner).await?;
+    server.api.db_add(owner, db, DbType::Mapped).await?;
+    let queries = &vec![QueryBuilder::insert()
+        .nodes()
+        .aliases("root")
+        .values(vec![vec![("key", 1.1).into()]])
+        .query()
+        .into()];
+    let (status, _) = server.api.db_exec(owner, db, queries).await?;
     assert_eq!(status, 200);
-    let queries: Option<Vec<QueryType>> =
-        Some(vec![QueryBuilder::select().ids("root").query().into()]);
-    let (status, responses) = server
-        .post(&format!("/db/{db}/exec"), &queries, &user.token)
-        .await?;
+    let queries = &vec![QueryBuilder::select().ids("root").query().into()];
+    let (status, results) = server.api.db_exec(owner, db, queries).await?;
     assert_eq!(status, 200);
-    let responses: Vec<QueryResult> = serde_json::from_str(&responses)?;
     let expected = vec![QueryResult {
         result: 1,
         elements: vec![DbElement {
@@ -90,29 +80,27 @@ async fn read_only() -> anyhow::Result<()> {
             values: vec![("key", 1.1).into()],
         }],
     }];
-
-    assert_eq!(responses.len(), expected.len());
-    assert_eq!(responses[0].result, expected[0].result);
-    assert_eq!(responses[0].elements, expected[0].elements);
-
+    assert_eq!(results, expected);
     Ok(())
 }
 
 #[tokio::test]
 async fn read_queries() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let db = server.init_db(DbType::Memory, &user).await?;
-    let queries: Option<Vec<QueryType>> = Some(vec![QueryBuilder::insert()
+    let owner = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.user_login(owner, owner).await?;
+    server.api.db_add(owner, db, DbType::Mapped).await?;
+    let queries = &vec![QueryBuilder::insert()
         .nodes()
         .aliases("node1")
         .values(vec![vec![("key", "value").into()]])
         .query()
-        .into()]);
-    server
-        .post(&format!("/db/{db}/exec"), &queries, &user.token)
-        .await?;
-    let queries: Option<Vec<QueryType>> = Some(vec![
+        .into()];
+    server.api.db_exec(owner, db, queries).await?;
+    let queries = &vec![
         QueryBuilder::search().from(1).query().into(),
         QueryBuilder::select().ids(1).query().into(),
         QueryBuilder::select().aliases().ids(1).query().into(),
@@ -125,23 +113,23 @@ async fn read_queries() -> anyhow::Result<()> {
             .ids(1)
             .query()
             .into(),
-    ]);
-    let (status, responses) = server
-        .post(&format!("/db/{db}/exec"), &queries, &user.token)
-        .await?;
+    ];
+    let (status, results) = server.api.db_exec(owner, db, queries).await?;
     assert_eq!(status, 200);
-    let responses: Vec<QueryResult> = serde_json::from_str(&responses)?;
-    assert_eq!(responses.len(), 8);
-
+    assert_eq!(results.len(), 8);
     Ok(())
 }
 
 #[tokio::test]
 async fn write_queries() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let db = server.init_db(DbType::Memory, &user).await?;
-    let queries: Option<Vec<QueryType>> = Some(vec![
+    let owner = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.user_login(owner, owner).await?;
+    server.api.db_add(owner, db, DbType::Mapped).await?;
+    let queries = &vec![
         QueryBuilder::insert().nodes().count(2).query().into(),
         QueryBuilder::insert()
             .aliases(vec!["node1", "node2"])
@@ -180,57 +168,52 @@ async fn write_queries() -> anyhow::Result<()> {
             .query()
             .into(),
         QueryBuilder::remove().ids("node1").query().into(),
-    ]);
-    let (status, responses) = server
-        .post(&format!("/db/{db}/exec"), &queries, &user.token)
-        .await?;
+    ];
+    let (status, results) = server.api.db_exec(owner, db, queries).await?;
     assert_eq!(status, 200);
-    let responses: Vec<QueryResult> = serde_json::from_str(&responses)?;
-    assert_eq!(responses.len(), 17);
-
+    assert_eq!(results.len(), 17);
     Ok(())
 }
 
 #[tokio::test]
 async fn query_error() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let db = server.init_db(DbType::Memory, &user).await?;
-    let queries: Option<Vec<QueryType>> = Some(vec![
+    let owner = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.user_login(owner, owner).await?;
+    server.api.db_add(owner, db, DbType::Mapped).await?;
+    let queries = &vec![
         QueryBuilder::insert()
             .nodes()
-            .aliases("")
             .values(vec![vec![("key", 1.1).into()]])
             .query()
             .into(),
         QueryBuilder::select().ids("root").query().into(),
-    ]);
-    let (status, response) = server
-        .post(&format!("/db/{db}/exec"), &queries, &user.token)
-        .await?;
-    assert_eq!(status, 470);
-    assert_eq!(response, "Alias 'root' not found");
-
+    ];
+    let error = server.api.db_exec(owner, db, queries).await.unwrap_err();
+    assert_eq!(error.status, 470);
+    assert_eq!(error.description, "Alias 'root' not found");
     Ok(())
 }
 
 #[tokio::test]
 async fn permission_denied() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let db = server.init_db(DbType::Memory, &user).await?;
-    let other = server.init_user().await?;
-    assert_eq!(
-        server
-            .put::<()>(
-                &format!("/db/{db}/user/{}/add?db_role=read", other.name),
-                &None,
-                &user.token
-            )
-            .await?,
-        201
-    );
-    let queries: Option<Vec<QueryType>> = Some(vec![
+    let owner = &server.next_user_name();
+    let user = &server.next_user_name();
+    let db = &server.next_db_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.admin_user_add(user, user).await?;
+    server.api.admin_db_add(owner, db, DbType::Mapped).await?;
+    server
+        .api
+        .admin_db_user_add(owner, db, user, DbUserRole::Read)
+        .await?;
+    server.api.user_login(user, user).await?;
+    let queries = &vec![
         QueryBuilder::insert()
             .nodes()
             .aliases("root")
@@ -238,39 +221,38 @@ async fn permission_denied() -> anyhow::Result<()> {
             .query()
             .into(),
         QueryBuilder::select().ids("root").query().into(),
-    ]);
-    let status = server
-        .post(&format!("/db/{db}/exec"), &queries, &other.token)
-        .await?
-        .0;
-    assert_eq!(status, 403);
-
+    ];
+    let error = server.api.db_exec(owner, db, queries).await.unwrap_err();
+    assert_eq!(error.status, 403);
     Ok(())
 }
 
 #[tokio::test]
 async fn db_not_found() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
-    let user = server.init_user().await?;
-    let queries: Option<Vec<QueryType>> = Some(vec![]);
+    let owner = &server.next_user_name();
+    server.api.user_login(ADMIN, ADMIN).await?;
+    server.api.admin_user_add(owner, owner).await?;
+    server.api.user_login(owner, owner).await?;
     let status = server
-        .post("/db/user/not_found/exec", &queries, &user.token)
-        .await?
-        .0;
+        .api
+        .db_exec(owner, "db", &vec![])
+        .await
+        .unwrap_err()
+        .status;
     assert_eq!(status, 404);
-
     Ok(())
 }
 
 #[tokio::test]
 async fn no_token() -> anyhow::Result<()> {
-    let mut server = TestServer::new().await?;
-    let queries: Option<Vec<QueryType>> = Some(vec![]);
+    let server = TestServer::new().await?;
     let status = server
-        .post("/db/user/not_found/exec", &queries, NO_TOKEN)
-        .await?
-        .0;
+        .api
+        .db_exec("user", "db", &vec![])
+        .await
+        .unwrap_err()
+        .status;
     assert_eq!(status, 401);
-
     Ok(())
 }

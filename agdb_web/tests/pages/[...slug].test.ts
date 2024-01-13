421e4a04d1f125bc349ae8ed508597d0e9d91005
Jana Vlachova <jana.vlach@gmail.com>
2024-01-13 10:43:49 +0100

    custom i18n

--- agdb_web/tests/pages/[...slug].test.ts
+++ agdb_web/tests/pages/[...slug].test.ts
@@ -1,6 +1,5 @@
 import { shallowMount } from "@vue/test-utils";
 import { describe, it, expect, vi, beforeEach } from "vitest";
-// import { useRoute } from "nuxt/app";
 import slug from "@/pages/[...slug].vue";
 
 describe("[...slug]", () => {

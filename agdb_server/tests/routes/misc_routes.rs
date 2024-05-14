79f7c4702d314646c3200ad3fbdf55ef94f6d7db
Michael Vlach <vlach.michael@gmail.com>
2024-05-14 20:59:07 +0200

    Update misc_routes.rs

--- agdb_server/tests/routes/misc_routes.rs
+++ agdb_server/tests/routes/misc_routes.rs
@@ -116,3 +116,33 @@ async fn db_list_after_shutdown() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn db_list_after_shutdown_corrupted_data() -> anyhow::Result<()> {
+    let mut server = TestServerImpl::new().await?;
+    let mut client = AgdbApi::new(ReqwestClient::new(), &server.address);
+
+    {
+        client.user_login(ADMIN, ADMIN).await?;
+        client.admin_user_add("userx", "userxpassword").await?;
+        client.user_logout().await?;
+        client.user_login("userx", "userxpassword").await?;
+        client
+            .db_add("userx", "mydb", agdb_api::DbType::Mapped)
+            .await?;
+        client.user_logout().await?;
+        client.user_login(ADMIN, ADMIN).await?;
+        client.admin_shutdown().await?;
+    }
+
+    std::fs::remove_dir_all(&server.data_dir)?;
+
+    server.process = Command::cargo_bin("agdb_server")?
+        .current_dir(&server.dir)
+        .spawn()?;
+    client.user_login("userx", "userxpassword").await?;
+    let dbs = client.db_list().await?.1;
+    assert_eq!(dbs.len(), 1);
+
+    Ok(())
+}

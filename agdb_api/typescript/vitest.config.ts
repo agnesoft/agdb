bd4e29d97afc4a733d9b6963083e6b721368dba2
Michael Vlach <vlach.michael@gmail.com>
2024-06-15 13:28:41 +0200

    [api] Fix coverage in agdb_typescript #1109 (#1116)
    * Update vitest.config.ts
    
    * Update InsertNodes.test.ts
    
    * Update InsertElements.test.ts

--- agdb_api/typescript/vitest.config.ts
+++ agdb_api/typescript/vitest.config.ts
@@ -8,6 +8,12 @@ export default mergeConfig(
             coverage: {
                 all: true,
                 exclude: [".eslintrc.cjs", "src/schema.d.ts"],
+                thresholds: {
+                    lines: 100,
+                    functions: 100,
+                    branches: 100,
+                    statements: 100,
+                },
             },
         },
     }),

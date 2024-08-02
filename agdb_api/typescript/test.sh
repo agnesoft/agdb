ce361637740c97ecf6d3a3291baad477257aa16b
Michael Vlach <vlach.michael@gmail.com>
2024-08-02 20:48:04 +0200

    Update test.sh

--- agdb_api/typescript/test.sh
+++ agdb_api/typescript/test.sh
@@ -5,15 +5,18 @@ rm -f agdb_server.agdb
 rm -rf agdb_server_data
 cargo build --release
 cargo run --release &
+
 cd ../agdb_api/typescript
-
 npx vitest run --coverage
-
 error_code=$?
+
 token=$(curl -X POST http://localhost:3000/api/v1/user/login -H "Content-Type: application/json" -d '{"username":"admin","password":"admin"}')
 curl -H "Authorization: Bearer $token" -X POST http://localhost:3000/api/v1/admin/shutdown
+
+cd ../../agdb_server
 rm -f agdb_server.yaml
 rm -f .agdb_server.agdb
 rm -f agdb_server.agdb
 rm -rf agdb_server_data
+
 exit $error_code

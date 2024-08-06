d54f51ade3bea82c96c937f846e2590ba8913991
Jana Vlachova <jana.vlach@gmail.com>
2024-08-06 17:35:49 +0200

    playwright config

--- agdb_web/playwright.config.ts
+++ agdb_web/playwright.config.ts
@@ -82,7 +82,7 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: "npm run start",
+        command: "npm run dev",
         url: "http://localhost:5001",
         reuseExistingServer: !process.env.CI,
     },

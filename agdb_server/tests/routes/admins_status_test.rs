8d99e986bb789c38b85e54a1b262e4ced5cbdf4a
Michael Vlach <vlach.michael@gmail.com>
2024-10-11 21:51:08 +0200

    Update admins_status_test.rs

--- agdb_server/tests/routes/admins_status_test.rs
+++ agdb_server/tests/routes/admins_status_test.rs
@@ -1,3 +1,5 @@
+use agdb_api::DbType;
+
 use crate::TestServer;
 use crate::ADMIN;
 
@@ -5,12 +7,16 @@ use crate::ADMIN;
 async fn status() -> anyhow::Result<()> {
     let mut server = TestServer::new().await?;
     server.api.user_login(ADMIN, ADMIN).await?;
+    server
+        .api
+        .admin_db_add(ADMIN, "status_db", DbType::Memory)
+        .await?;
 
     let (status, admin_status) = server.api.admin_status().await?;
     assert_eq!(status, 200);
-    assert_eq!(admin_status.dbs, 0);
-    assert_eq!(admin_status.users, 1);
-    assert_eq!(admin_status.logged_in_users, 1);
+    assert_ne!(admin_status.dbs, 0);
+    assert_ne!(admin_status.users, 0);
+    assert_ne!(admin_status.logged_in_users, 0);
     assert_ne!(admin_status.size, 0);
 
     Ok(())

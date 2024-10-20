722992067c6bdb23a2f8e3558610acd3f6bafe81
Jana Vlachova <jana.vlach@gmail.com>
2024-10-20 17:04:13 +0200

    studio - config

--- agdb_studio/vite.config.ts
+++ agdb_studio/vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
+    esbuild: {
+        supported: {
+            "top-level-await": true,
+        },
+    },
 });

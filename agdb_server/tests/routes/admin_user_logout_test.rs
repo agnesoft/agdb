d2a17c4d85fdf18cf9522df41739a8a32768b41f
Michael Vlach <vlach.michael@gmail.com>
2024-12-22 13:42:33 +0100

    Update admin_user_logout_test.rs

--- agdb_server/tests/routes/admin_user_logout_test.rs
+++ agdb_server/tests/routes/admin_user_logout_test.rs
@@ -1,4 +1,5 @@
 use crate::next_user_name;
+use crate::TestCluster;
 use crate::TestServer;
 use crate::ADMIN;
 
@@ -57,6 +58,28 @@ async fn no_token() -> anyhow::Result<()> {
         .unwrap_err()
         .status;
     assert_eq!(status, 401);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn cluster_user_logout() -> anyhow::Result<()> {
+    let mut cluster = TestCluster::new().await?;
+    let user = &next_user_name();
+
+    {
+        let client = cluster.apis.get_mut(1).unwrap();
+        client.cluster_login(ADMIN, ADMIN).await?;
+        client.admin_user_add(user, user).await?;
+        client.cluster_login(user, user).await?;
+    }
+
+    cluster.apis[1].user_status().await?;
+    let leader = cluster.apis.get_mut(0).unwrap();
+    leader.user_login(ADMIN, ADMIN).await?;
+    leader.admin_cluster_logout(user).await?;
+
+    assert_eq!(cluster.apis[1].user_status().await.unwrap_err().status, 401);
 
     Ok(())
 }

afc554dfa9a38e93873b42b700b5b1816baffdfb
Jana Vlachová <65499282+janavlachova@users.noreply.github.com>
2024-12-23 11:09:14 +0100

    [web] fix locale redirect #1397 (#1402)
    web - fix locale redirect

--- agdb_web/pages/cs-CZ/_meta.ts
+++ agdb_web/pages/cs-CZ/_meta.ts
@@ -7,38 +7,8 @@ const config = {
             breadcrumb: false,
         },
     },
-    "api-docs": {
-        title: "API",
-        type: "page",
-        display: "normal",
-        theme: {
-            sidebar: true,
-            toc: true,
-            breadcrumb: true,
-        },
-    },
     docs: {
         title: "Docs",
-        type: "page",
-        display: "normal",
-        theme: {
-            sidebar: true,
-            toc: true,
-            breadcrumb: true,
-        },
-    },
-    enterprise: {
-        title: "Enterprise",
-        type: "page",
-        display: "normal",
-        theme: {
-            sidebar: true,
-            toc: true,
-            breadcrumb: true,
-        },
-    },
-    blog: {
-        title: "Blog",
         type: "page",
         display: "normal",
         theme: {

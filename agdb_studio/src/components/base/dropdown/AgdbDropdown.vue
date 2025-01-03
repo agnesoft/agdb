22178fb1b205466935215f061d6b8c052d7ded2e
Jana Vlachová <65499282+janavlachova@users.noreply.github.com>
2025-01-03 16:53:40 +0100

    [studio] fix dropdown positioning #1452 (#1453)
    studio - fixed dropdown positioning

--- agdb_studio/src/components/base/dropdown/AgdbDropdown.vue
+++ agdb_studio/src/components/base/dropdown/AgdbDropdown.vue
@@ -1,30 +1,39 @@
 <script lang="ts" setup>
 import { vOnClickOutside } from "@vueuse/components";
 import { ref } from "vue";
+import DropdownContent from "./DropdownContent.vue";
 
 const opened = ref(false);
 const close = () => {
-    if (!opened.value) return;
     opened.value = false;
 };
 const toggle = () => {
     opened.value = !opened.value;
 };
+const buttonRef = ref<HTMLElement>();
 </script>
 
 <template>
-    <div class="agdb-dropdown" v-on-click-outside="close">
-        <button type="button" class="trigger button" @click="toggle">
+    <div class="agdb-dropdown">
+        <button
+            type="button"
+            class="trigger button"
+            @click="toggle"
+            ref="buttonRef"
+        >
             <slot name="trigger"></slot>
         </button>
-        <div v-if="opened" class="content" @click="close">
-            <slot name="content"></slot>
-        </div>
+        <Teleport to="body">
+            <DropdownContent
+                :button-ref="buttonRef"
+                :opened="opened"
+                v-on-click-outside="close"
+                @close="close"
+            >
+                <slot name="content"></slot>
+            </DropdownContent>
+        </Teleport>
     </div>
 </template>
 
-<style lang="less" scoped>
-.content {
-    position: absolute;
-}
-</style>
+<style lang="less" scoped></style>

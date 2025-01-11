c000eb3a8b917cfeabf87958158158e352c7dac9
Jana Vlachová <jana.vlach@gmail.com>
2025-01-11 09:16:23 +0100

    studio - unit test

--- agdb_studio/src/components/db/DbDetails.spec.ts
+++ agdb_studio/src/components/db/DbDetails.spec.ts
@@ -1,12 +1,31 @@
 import { mount } from "@vue/test-utils";
 import { describe, beforeEach, vi, it, expect } from "vitest";
 import DbDetails from "./DbDetails.vue";
+import { db_user_list, db_user_add, db_user_remove } from "@/tests/apiMock";
 
 describe("DbDetails", () => {
     beforeEach(() => {
         vi.clearAllMocks();
     });
-    it("should render", async () => {
-        expect(true).toBe(true);
+    it("should render users", async () => {
+        db_user_list.mockResolvedValue({
+            data: [
+                {
+                    username: "test",
+                    role: "read",
+                },
+            ],
+        });
+        const wrapper = mount(DbDetails, {
+            props: {
+                db: {
+                    owner: "test",
+                    db: "test",
+                },
+            },
+        });
+        await wrapper.vm.$nextTick();
+        expect(wrapper.text()).toContain("test");
+        expect(wrapper.text()).toContain("(R)");
     });
 });

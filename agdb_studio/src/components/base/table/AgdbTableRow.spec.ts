e63eef36767915c3e10d9a64fd8de69674dd9383
Jana Vlachová <jana.vlach@gmail.com>
2025-01-11 16:05:22 +0100

    studio - dbdetails stub in unit test

--- agdb_studio/src/components/base/table/AgdbTableRow.spec.ts
+++ agdb_studio/src/components/base/table/AgdbTableRow.spec.ts
@@ -59,6 +59,7 @@ describe("TableRow", () => {
                 },
                 stubs: {
                     transitions: false,
+                    DbDetails: true,
                 },
             },
         });

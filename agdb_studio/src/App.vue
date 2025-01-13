d9d401ce804060bc83037ea8343091b8441c371b
Jana Vlachová <jana.vlach@gmail.com>
2025-01-13 20:11:37 +0100

    studio - scrollbar style

--- agdb_studio/src/App.vue
+++ agdb_studio/src/App.vue
@@ -9,5 +9,6 @@ import { RouterView } from "vue-router";
 <style lang="less">
 @import "@/assets/button.less";
 @import "@/assets/form.less";
+@import "@/assets/scrollbar.less";
 </style>
 <style scoped></style>

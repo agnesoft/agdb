c2857f86e302f7990f14d07ea04a1b06b530a4ad
Michael Vlach <vlach.michael@gmail.com>
2025-01-19 20:20:21 +0100

    Update intro.tsx

--- agdb_web/components/pages/intro/intro.tsx
+++ agdb_web/components/pages/intro/intro.tsx
@@ -4,10 +4,12 @@ export const Intro = () => {
     return (
         <div className={styles.intro}>
             <h1>agdb</h1>
-            <p className={styles.subheading}>The graph database.</p>
+            <p className={styles.subheading}>First programming native database without compromises.</p>
+            <p className={styles.subheading}><a href="/whyagdb#no-query-language">no query language</a> | <a href="/whyagdb#performance">performance independnet of data size</a> | <a href="/whyagdb#graph">10X cost reduction</a></p>
             <p>
                 <a href="/docs/guides/quickstart">Quickstart</a>
                 <a href="/blog/blog">Why agdb?</a>
+                <a href="/consulation">Expert consultation</a>
             </p>
         </div>
     );

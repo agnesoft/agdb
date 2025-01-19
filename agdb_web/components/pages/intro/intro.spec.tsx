3e7852a964186a46ac9cd2a910ddd13998f32d8e
Michael Vlach <vlach.michael@gmail.com>
2025-01-19 21:27:45 +0100

    Update intro.spec.tsx

--- agdb_web/components/pages/intro/intro.spec.tsx
+++ agdb_web/components/pages/intro/intro.spec.tsx
@@ -5,6 +5,10 @@ import Intro from "@/components/pages/intro";
 describe("intro", () => {
     it("should render the intro", () => {
         render(<Intro />);
-        expect(screen.getByText(/First application native database without compromises/i)).toBeDefined();
+        expect(
+            screen.getByText(
+                /First application native database without compromises/i,
+            ),
+        ).toBeDefined();
     });
 });

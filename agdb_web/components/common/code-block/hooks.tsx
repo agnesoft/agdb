0611411bdd06c4fc8355eb08c5590f124ea99edd
Michael Vlach <vlach.michael@gmail.com>
2025-01-22 20:05:06 +0100

    fix lints

--- agdb_web/components/common/code-block/hooks.tsx
+++ agdb_web/components/common/code-block/hooks.tsx
@@ -1,4 +1,10 @@
 import hljs from "highlight.js/lib/core";
+import rust from "highlight.js/lib/languages/rust";
+import json from "highlight.js/lib/languages/json";
+import python from "highlight.js/lib/languages/python";
+import php from "highlight.js/lib/languages/php";
+import javascript from "highlight.js/lib/languages/javascript";
+import typescript from "highlight.js/lib/languages/typescript";
 
 export const useHighlight = () => {
     const highlight = (code: HTMLElement) => {
@@ -10,40 +16,22 @@ export const useHighlight = () => {
     const setLanguage = (language: string) => {
         switch (language) {
             case "json":
-                hljs.registerLanguage(
-                    "json",
-                    require("highlight.js/lib/languages/json"),
-                );
+                hljs.registerLanguage("json", json);
                 break;
             case "rust":
-                hljs.registerLanguage(
-                    "rust",
-                    require("highlight.js/lib/languages/rust"),
-                );
+                hljs.registerLanguage("rust", rust);
                 break;
             case "python":
-                hljs.registerLanguage(
-                    "python",
-                    require("highlight.js/lib/languages/python"),
-                );
+                hljs.registerLanguage("python", python);
                 break;
             case "php":
-                hljs.registerLanguage(
-                    "php",
-                    require("highlight.js/lib/languages/php"),
-                );
+                hljs.registerLanguage("php", php);
                 break;
             case "javascript":
-                hljs.registerLanguage(
-                    "javascript",
-                    require("highlight.js/lib/languages/javascript"),
-                );
+                hljs.registerLanguage("javascript", javascript);
                 break;
             case "typescript":
-                hljs.registerLanguage(
-                    "typescript",
-                    require("highlight.js/lib/languages/typescript"),
-                );
+                hljs.registerLanguage("typescript", typescript);
                 break;
             default:
                 break;
--- agdb_web/components/pages/openapi/openapi.spec.tsx
+++ agdb_web/components/pages/openapi/openapi.spec.tsx
@@ -27,7 +27,7 @@ describe("openapi", () => {
     it("should render the openapi code on click", async () => {
         const file = await import("../../../../agdb_server/openapi.json");
 
-        //@ts-ignore
+        //@ts-expect-error no types for default
         file.default = jsonMock;
 
         render(<OpenApi />);

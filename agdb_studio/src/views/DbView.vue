3462fac54b69c57a22a5f31f05bbac4f06facbae
Jana Vlachová <jana.vlach@gmail.com>
2025-01-23 19:47:15 +0100

    studio - db view

--- agdb_studio/src/views/DbView.vue
+++ agdb_studio/src/views/DbView.vue
@@ -3,6 +3,7 @@ import { useDbStore } from "@/composables/db/dbStore";
 import { onMounted } from "vue";
 import DbAddForm from "@/components/db/DbAddForm.vue";
 import DbTable from "@/components/db/DbTable.vue";
+import { JaRefreshReverse } from "@kalimahapps/vue-icons";
 
 const { fetchDatabases } = useDbStore();
 onMounted(async () => {
@@ -12,8 +13,12 @@ onMounted(async () => {
 
 <template>
     <div class="db-view">
-        <button class="button" @click="fetchDatabases">Refresh</button>
-        <DbAddForm />
+        <div class="header">
+            <DbAddForm />
+            <button class="button" @click="fetchDatabases" title="refresh">
+                <JaRefreshReverse />
+            </button>
+        </div>
         <DbTable />
     </div>
 </template>
@@ -21,5 +26,29 @@ onMounted(async () => {
 <style lang="less" scoped>
 .db-view {
     text-align: center;
+}
+.header {
+    display: grid;
+    justify-content: space-between;
+    align-items: center;
+    grid-template-columns: 2rem 1fr 2rem;
+    grid-template-areas: ". form refresh";
+    max-width: 1200px;
+    margin: 0 auto;
+    .button {
+        width: 2rem;
+        height: 2rem;
+        font-size: 1rem;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .db-add-form {
+        grid-area: form;
+    }
+    .button {
+        grid-area: refresh;
+    }
 }
 </style>

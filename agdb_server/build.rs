3344f0b5456ed0c049d82770c113bd8a93dfcbf9
Michael Vlach <vlach.michael@gmail.com>
2025-01-27 18:29:49 +0100

    Update build.rs

--- agdb_server/build.rs
+++ agdb_server/build.rs
@@ -1,35 +1,36 @@
-use std::process::Command;
-
 fn build_studio() {
+    println!("cargo::rerun-if-changed=../agdb_api/typescript");
+    println!("cargo::rerun-if-changed=../agdb_studio");
+
     #[cfg(feature = "studio")]
-    {
+    if std::env::var("AGDB_DOCKER_BUILD").is_err() {
         #[cfg(target_os = "windows")]
         const BASH: &str = "C:/Program Files/Git/bin/bash.exe";
         #[cfg(not(target_os = "windows"))]
         const BASH: &str = "bash";
 
-        Command::new(BASH)
+        std::process::Command::new(BASH)
             .arg("-c")
             .arg("npm ci")
             .current_dir("../agdb_api/typescript")
             .output()
             .unwrap();
 
-        Command::new(BASH)
+        std::process::Command::new(BASH)
             .arg("-c")
             .arg("npm build")
             .current_dir("../agdb_api/typescript")
             .output()
             .unwrap();
 
-        Command::new(BASH)
+        std::process::Command::new(BASH)
             .arg("-c")
             .arg("npm ci")
             .current_dir("../agdb_studio")
             .output()
             .unwrap();
 
-        Command::new(BASH)
+        std::process::Command::new(BASH)
             .arg("-c")
             .arg("npm build")
             .current_dir("../agdb_studio")
@@ -39,10 +40,5 @@ fn build_studio() {
 }
 
 fn main() {
-    println!("cargo::rerun-if-changed=../agdb_api/typescript");
-    println!("cargo::rerun-if-changed=../agdb_studio");
-
-    if std::env::var("AGDB_DOCKER_BUILD").is_err() {
-        build_studio();
-    }
+    build_studio();
 }

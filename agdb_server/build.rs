3ebfa440ce0c8881e1d2ab3e28e8c0d705f77e18
Michael Vlach <vlach.michael@gmail.com>
2025-01-27 18:54:51 +0100

    Update build.rs

--- agdb_server/build.rs
+++ agdb_server/build.rs
@@ -1,21 +1,20 @@
 use std::io::Write;
-use std::path::Path;
 use std::process::Command;
 
-#[allow(dead_code)]
-fn run_command(command: &mut Command) {
-    let out = command.output().unwrap();
+#[cfg(target_os = "windows")]
+const BASH: &str = "C:/Program Files/Git/bin/bash.exe";
+#[cfg(not(target_os = "windows"))]
+const BASH: &str = "bash";
+
+fn run_command(command: &str, dir: &str) {
+    let out = Command::new(BASH)
+        .arg("-c")
+        .arg(command)
+        .current_dir(dir)
+        .output()
+        .unwrap();
     std::io::stdout().write_all(&out.stdout).unwrap();
     std::io::stderr().write_all(&out.stderr).unwrap();
-    if !out.status.success() {
-        println!(
-            "Command failed: {command:?} ({})",
-            command
-                .get_current_dir()
-                .unwrap_or(Path::new("."))
-                .to_string_lossy()
-        );
-    }
 }
 
 #[allow(dead_code)]
@@ -24,38 +23,8 @@ fn build_studio() {
     println!("cargo::rerun-if-changed=../agdb_studio");
 
     if std::env::var("AGDB_DOCKER_BUILD").is_err() {
-        #[cfg(target_os = "windows")]
-        const BASH: &str = "C:/Program Files/Git/bin/bash.exe";
-        #[cfg(not(target_os = "windows"))]
-        const BASH: &str = "bash";
-
-        run_command(
-            Command::new(BASH)
-                .arg("-c")
-                .arg("npm ci")
-                .current_dir("../agdb_api/typescript"),
-        );
-
-        run_command(
-            Command::new(BASH)
-                .arg("-c")
-                .arg("npm run build")
-                .current_dir("../agdb_api/typescript"),
-        );
-
-        run_command(
-            Command::new(BASH)
-                .arg("-c")
-                .arg("npm ci")
-                .current_dir("../agdb_studio"),
-        );
-
-        run_command(
-            Command::new(BASH)
-                .arg("-c")
-                .arg("npm run build")
-                .current_dir("../agdb_studio"),
-        );
+        run_command("npm ci && npm run build", "../agdb_api/typescript");
+        run_command("npm ci && npm run build", "../agdb_studio");
     }
 }
 

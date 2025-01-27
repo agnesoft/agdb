eed182c027417f0f3eb94040fce89dd7121c2d63
Michael Vlach <vlach.michael@gmail.com>
2025-01-27 19:22:24 +0100

    [server] Create build script to prebuild studio of the feature is enabled #1526 (#1541)
    * update flows
    
    * Update build.rs
    
    * Update build.rs
    
    * Update build.rs
    
    * Update build.rs

---
+++ agdb_server/build.rs
@@ -0,0 +1,34 @@
+use std::io::Write;
+use std::process::Command;
+
+#[cfg(target_os = "windows")]
+const BASH: &str = "C:/Program Files/Git/bin/bash.exe";
+#[cfg(not(target_os = "windows"))]
+const BASH: &str = "bash";
+
+fn run_command(command: &str, dir: &str) {
+    let out = Command::new(BASH)
+        .arg("-c")
+        .arg(command)
+        .current_dir(dir)
+        .output()
+        .unwrap();
+    std::io::stdout().write_all(&out.stdout).unwrap();
+    std::io::stderr().write_all(&out.stderr).unwrap();
+}
+
+#[allow(dead_code)]
+fn build_studio() {
+    println!("cargo::rerun-if-changed=../agdb_api/typescript");
+    println!("cargo::rerun-if-changed=../agdb_studio");
+
+    if std::env::var("AGDB_DOCKER_BUILD").is_err() {
+        run_command("npm ci && npm run build", "../agdb_api/typescript");
+        run_command("npm ci && npm run build", "../agdb_studio");
+    }
+}
+
+fn main() {
+    #[cfg(feature = "studio")]
+    build_studio();
+}

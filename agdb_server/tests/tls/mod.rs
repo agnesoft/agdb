237bf5173b05af4244c74b3639b3d8e141a9e367
Michael Vlach <vlach.michael@gmail.com>
2025-03-01 22:37:33 +0100

    configurable request body limit

--- agdb_server/tests/tls/mod.rs
+++ agdb_server/tests/tls/mod.rs
@@ -5,6 +5,8 @@ use crate::wait_for_ready;
 use crate::ConfigImpl;
 use crate::TestServerImpl;
 use crate::ADMIN;
+use crate::DEFAULT_LOG_BODY_LIMIT;
+use crate::DEFAULT_REQUEST_BODY_LIMIT;
 use crate::SERVER_DATA_DIR;
 use agdb_api::AgdbApi;
 use agdb_api::ReqwestClient;
@@ -20,6 +22,8 @@ async fn https() -> anyhow::Result<()> {
         basepath: String::new(),
         admin: ADMIN.to_string(),
         log_level: LevelFilter::INFO,
+        log_body_limit: DEFAULT_LOG_BODY_LIMIT,
+        request_body_limit: DEFAULT_REQUEST_BODY_LIMIT,
         data_dir: SERVER_DATA_DIR.into(),
         pepper_path: String::new(),
         tls_certificate: format!("{manifest_dir}/tests/test_cert.pem"),

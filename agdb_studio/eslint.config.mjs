b23830e1e7e7eb85cf502bf69434938071546ee4
Michael Vlach <vlach.michael@gmail.com>
2025-05-10 22:20:02 +0200

    [ci] Release 0.11.1 #1580 (#1581)
    * update version
    
    * Update eslint.config.mjs

--- agdb_studio/eslint.config.mjs
+++ agdb_studio/eslint.config.mjs
@@ -1,7 +1,6 @@
 import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginVue from "eslint-plugin-vue";
-import globals from "globals";
 import typescriptEslint from "typescript-eslint";
 
 export default typescriptEslint.config(
@@ -16,7 +15,6 @@ export default typescriptEslint.config(
         languageOptions: {
             ecmaVersion: "latest",
             sourceType: "module",
-            globals: globals.browser,
             parserOptions: {
                 parser: typescriptEslint.parser,
             },

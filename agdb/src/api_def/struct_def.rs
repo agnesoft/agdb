2673dda8ce67a409a6412ca97bde97060da1395d
Michael Vlach <vlach.michael@gmail.com>
2025-12-21 10:45:17 +0100

    Update struct_def.rs

--- agdb/src/api_def/struct_def.rs
+++ agdb/src/api_def/struct_def.rs
@@ -73,7 +73,7 @@ mod tests {
             assert_eq!(s.generics.len(), 1);
             assert_eq!(s.generics[0].name, "T");
             assert_eq!(s.generics[0].bounds.len(), 1);
-            assert_eq!((s.generics[0].bounds[0])().name, "Bound5");
+            assert_eq!(s.generics[0].bounds[0], "Bound5");
         } else {
             panic!("Expected Type::Struct");
         }
